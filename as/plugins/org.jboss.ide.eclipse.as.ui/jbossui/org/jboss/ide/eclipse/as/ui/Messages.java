9295154acfd789eb5ea512ea3f5fefebffd223ca
Rob Stryker <rob.stryker@jboss.com>
2006-06-20 16:08:49 +0000

    Some strings
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@1098 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ as/plugins/org.jboss.ide.eclipse.as.ui/jbossui/org/jboss/ide/eclipse/as/ui/Messages.java
@@ -0,0 +1,61 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2006, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.ide.eclipse.as.ui;
+
+public class Messages {
+	public static final String ServerStateStarting 	= "%ServerStateStarting";
+	public static final String ServerStateStarted 	= "%ServerStateStarted";
+	public static final String ServerStateStopping 	= "%ServerStateStopping";
+	public static final String ServerStateStopped 	= "%ServerStateStopped";
+	public static final String ServerStateUnknown 	= "%ServerStateUnknown";
+	
+	
+	public static final String EventLogCategory = "%EventLog";
+	public static final String ModulesCategory = "%ModulesCategory";
+	
+	public static final String viewServer 	= "%ViewServer";
+	public static final String viewStatus 	= "%ViewStatus";
+	public static final String viewSync 	= "%ViewSync";
+
+	public static final String viewSyncOkay = "%viewSyncOkay";
+	public static final String viewSyncRestart= "%viewSyncRestart";
+	public static final String viewSyncPublish = "%viewSyncPublish";
+	public static final String viewSyncRestartPublish = "%viewSyncRestartPublish";
+	public static final String viewSyncPublishing = "%viewSyncPublishing";
+	
+	
+	
+	
+	/*
+	  To be added:
+	  	pluginName=JBoss WTP Server Tools UI Plugin
+
+
+
+		createWizardTitle=Create a new JBoss Server
+		createWizardDescription=Add a JBoss Server and have great fun
+		wizardFragmentNameLabel=Name (unique identifier)
+		wizardFragmentHomeDirLabel=Home Directory
+		wizardFragmentJRELabel=JRE
+		wizardFragmentConfigLabel=Configuration
+	 */
+}
