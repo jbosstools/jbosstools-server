4594f219887f9653c4bef8b0171540457b1bef45
Rob Stryker <rob.stryker@jboss.com>
2007-05-01 21:58:32 +0000

    ensuring wtp projects have package types correct
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@1986 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/packages/types/EjbArchiveType.java
+++ as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/packages/types/EjbArchiveType.java
@@ -1,7 +1,11 @@
 package org.jboss.ide.eclipse.as.core.packages.types;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.wst.server.core.IModule;
 import org.jboss.ide.eclipse.archives.core.model.IArchive;
 
 public class EjbArchiveType extends J2EEArchiveType {
@@ -21,11 +25,27 @@ public class EjbArchiveType extends J2EEArchiveType {
 
 	public IArchive createDefaultConfiguration(IProject project, IProgressMonitor monitor) {
 		IArchive topLevel = createGenericIArchive(project, null, project.getName() + ".jar");
+		fillDefaultConfiguration(project, topLevel, monitor); 
 		return fillDefaultConfiguration(project, topLevel, monitor);
 	}
 
 	public IArchive fillDefaultConfiguration(IProject project, IArchive topLevel, IProgressMonitor monitor) {
+		IModule mod = getModule(project);
 		
+		// TODO:  module artifact to adapt a jboss ejb 30 to a module is MIA. CREATE IT
+		if( mod == null ) {
+			
+		} else {
+			if( mod.getModuleType().getId().equals("jst.ejb")
+					|| mod.getModuleType().getId().equals("jbide.ejb30")) {
+				try {
+					IJavaProject proj = JavaCore.create(project);
+					IPath outputLoc = proj.getOutputLocation();
+					addFileset(project, topLevel, outputLoc.toOSString(), null);
+				} catch( Exception e ) {
+				}
+			}
+		}
 		return topLevel;
 	}
 
