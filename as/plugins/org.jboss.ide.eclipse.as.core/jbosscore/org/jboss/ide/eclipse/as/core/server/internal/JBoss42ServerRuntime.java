712f7013a500a374b4f1aede10145a8f908090b4
Marshall Culpepper <mculpepper@jboss.com>
2007-11-27 20:29:03 +0000

    tagging for 2.0.0.GA
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags/jbosstools-2.0.0.GA@5127 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/JBoss42ServerRuntime.java
@@ -0,0 +1,9 @@
+package org.jboss.ide.eclipse.as.core.server.internal;
+
+public class JBoss42ServerRuntime extends AbstractJBossServerRuntime {
+	public JBoss42ServerRuntime() {
+	}
+	public String getId() {
+		return "4.2";
+	}
+}
---
+++ as/plugins/org.jboss.ide.eclipse.as.ui.mbeans/src/org/jboss/ide/eclipse/as/ui/mbeans/packages/NewSARAction.java
@@ -0,0 +1,16 @@
+package org.jboss.ide.eclipse.as.ui.mbeans.packages;
+
+import org.jboss.ide.eclipse.ui.util.ActionWithDelegate;
+
+public class NewSARAction extends ActionWithDelegate {
+
+	public NewSARAction() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
