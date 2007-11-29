3e1822fc122260dd9df45933b05b02e29da47ba7
Marshall Culpepper <mculpepper@jboss.com>
2007-11-29 00:16:24 +0000

    retagging as
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags/jbosstools-2.0.0.GA@5145 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/JBoss42ServerRuntime.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.ide.eclipse.as.core.server.internal;
-
-public class JBoss42ServerRuntime extends AbstractJBossServerRuntime {
-	public JBoss42ServerRuntime() {
-	}
-	public String getId() {
-		return "4.2";
-	}
-}
