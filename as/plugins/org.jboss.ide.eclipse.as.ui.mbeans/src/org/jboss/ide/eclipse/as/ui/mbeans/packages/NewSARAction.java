a9e7cb6df88877c30cf7cdcd895be93f966c7e82
Rob Stryker <rob.stryker@jboss.com>
2008-02-07 22:17:07 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6188 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.ui.mbeans/src/org/jboss/ide/eclipse/as/ui/mbeans/packages/NewSARAction.java
+++ as/plugins/org.jboss.ide.eclipse.as.ui.mbeans/src/org/jboss/ide/eclipse/as/ui/mbeans/packages/NewSARAction.java
@@ -1,8 +1,7 @@
 package org.jboss.ide.eclipse.as.ui.mbeans.packages;
 
-import org.jboss.ide.eclipse.ui.util.ActionWithDelegate;
 
-public class NewSARAction extends ActionWithDelegate {
+public class NewSARAction { //extends ActionWithDelegate {
 
 	public NewSARAction() {
 		// TODO Auto-generated constructor stub
