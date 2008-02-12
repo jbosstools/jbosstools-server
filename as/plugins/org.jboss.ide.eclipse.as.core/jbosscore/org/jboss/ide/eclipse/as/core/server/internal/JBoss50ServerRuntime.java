a9640d51f524800fca7b8c20efb365511aeff19a
Rob Stryker <rob.stryker@jboss.com>
2008-02-12 19:42:39 +0000

    JBIDE-1774
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6282 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/JBoss50ServerRuntime.java
@@ -0,0 +1,9 @@
+package org.jboss.ide.eclipse.as.core.server.internal;
+
+public class JBoss50ServerRuntime extends AbstractJBossServerRuntime {
+	public JBoss50ServerRuntime() {
+	}
+	public String getId() {
+		return "5.0";
+	}
+}
