75509af962546640f3516e8198cda58a2f19b4ab
Rob Stryker <rob.stryker@jboss.com>
2008-12-04 19:19:55 +0000

    JBIDE-3342
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@12288 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.ui/jbossui/org/jboss/ide/eclipse/as/ui/IPreferenceKeys.java
+++ as/plugins/org.jboss.ide.eclipse.as.ui/jbossui/org/jboss/ide/eclipse/as/ui/IPreferenceKeys.java
@@ -2,5 +2,5 @@ package org.jboss.ide.eclipse.as.ui;
 
 
 public interface IPreferenceKeys {
-	public static final String ENABLED_DECORATORS = "org.jboss.ide.eclipse.as.ui.preferences.enabledDecorators.3.0.0.CR1";
+	public static final String ENABLED_DECORATORS = "org.jboss.ide.eclipse.as.ui.preferences.enabledDecorators.1";
 }
