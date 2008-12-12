0df8a1c30be9f10687894fcf8c4d1f1fe88249b2
Nick Boldt <nboldt@redhat.com>
2008-12-12 21:06:45 +0000

    [JBIDE-3191] add AllTests suite to test plugins
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@12578 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jmx/tests/org.jboss.tools.jmx.core.test/src/org/jboss/tools/jmx/core/AllTests.java
@@ -0,0 +1,9 @@
+package org.jboss.tools.jmx.core;
+import junit.framework.Test;
+
+
+public class AllTests {
+	public static Test suite() {
+		return AllTestsUtil.suite();
+	}
+}
---
+++ jmx/tests/org.jboss.tools.jmx.ui.test/src/org/jboss/tools/jmx/ui/AllTests.java
@@ -0,0 +1,9 @@
+package org.jboss.tools.jmx.ui;
+import junit.framework.Test;
+
+
+public class AllTests {
+	public static Test suite() {
+		return AllTestsUtil.suite();
+	}
+}
