d58f874deec3f4d98b1f52260527e68beaf6ae8c
Rob Stryker <rob.stryker@jboss.com>
2008-12-15 17:52:07 +0000

    JBIDE-3401 - manually setting test cases
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@12600 a97e2381-89e5-4abb-bab3-167db6db766c

--- jmx/tests/org.jboss.tools.jmx.core.test/src/org/jboss/tools/jmx/core/AllTests.java
+++ jmx/tests/org.jboss.tools.jmx.core.test/src/org/jboss/tools/jmx/core/AllTests.java
@@ -1,9 +1,18 @@
 package org.jboss.tools.jmx.core;
 import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.jboss.tools.jmx.core.tests.DefaultMBeanServerConnectionFactoryTest;
+import org.jboss.tools.jmx.core.tests.DefaultProviderTest;
+import org.jboss.tools.jmx.core.tests.NodeBuilderTestCase;
 
 
 public class AllTests {
 	public static Test suite() {
-		return AllTestsUtil.suite();
+		TestSuite suite = new TestSuite();
+		suite.addTestSuite(DefaultMBeanServerConnectionFactoryTest.class);
+		suite.addTestSuite(DefaultProviderTest.class);
+		suite.addTestSuite(NodeBuilderTestCase.class);
+		return suite;
 	}
 }
--- jmx/tests/org.jboss.tools.jmx.ui.test/src/org/jboss/tools/jmx/ui/AllTests.java
+++ jmx/tests/org.jboss.tools.jmx.ui.test/src/org/jboss/tools/jmx/ui/AllTests.java
@@ -1,9 +1,14 @@
 package org.jboss.tools.jmx.ui;
+import org.jboss.tools.jmx.ui.internal.MBeanUtilsTestCase;
+
 import junit.framework.Test;
+import junit.framework.TestSuite;
 
 
 public class AllTests {
 	public static Test suite() {
-		return AllTestsUtil.suite();
+		TestSuite suite = new TestSuite();
+		suite.addTestSuite(MBeanUtilsTestCase.class);
+		return suite;
 	}
 }
