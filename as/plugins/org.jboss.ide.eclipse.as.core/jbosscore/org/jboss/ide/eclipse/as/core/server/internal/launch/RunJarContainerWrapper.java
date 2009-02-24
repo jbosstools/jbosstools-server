fb115c840cc2b1bfe692d567c430772b6e2d2257
Rob Stryker <rob.stryker@jboss.com>
2009-02-24 05:17:31 +0000

    JBIDE-3770 - changed ids to non-internal
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@13741 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/launch/RunJarContainerWrapper.java
+++ as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/launch/RunJarContainerWrapper.java
@@ -17,7 +17,7 @@ import org.eclipse.wst.server.core.ServerCore;
 
 public class RunJarContainerWrapper {
 	public static final String ID = "org.jboss.ide.eclipse.as.core.server.launch.runJarContainer";
-	public static final String RESOLVER_ID = "org.jboss.ide.eclipse.as.core.server.internal.launch.runtimeCPEResolver";
+	public static final String RESOLVER_ID = "org.jboss.ide.eclipse.as.core.server.launch.runJarContainer.resolver";
 
 	public static class RunJarContainerInitializer extends ClasspathContainerInitializer { 
 		public void initialize(IPath containerPath, IJavaProject project)
