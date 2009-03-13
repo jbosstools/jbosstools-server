1427b65eebd4672b69204cfeb9950f815f54d0b3
Rob Stryker <rob.stryker@jboss.com>
2009-03-13 09:29:16 +0000

    Added an extra class extenders can use as their label provider
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@14205 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jmx/plugins/org.jboss.tools.jmx.ui/src/org/jboss/tools/jmx/ui/internal/views/navigator/EmbedableLabelProvider.java
@@ -0,0 +1,27 @@
+package org.jboss.tools.jmx.ui.internal.views.navigator;
+
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.jboss.tools.jmx.core.IConnectionWrapper;
+
+public class EmbedableLabelProvider extends LabelProvider {
+	private MBeanExplorerLabelProvider delegate;
+	public EmbedableLabelProvider() {
+		delegate = new MBeanExplorerLabelProvider();
+	}
+	
+	public void dispose() {
+		delegate.dispose();
+	}
+	
+	public String getText(Object element) {
+		if( element instanceof IConnectionWrapper ) {
+			return "JMX"; 
+		}
+		return delegate.getText(element);
+	}
+	
+	public Image getImage(Object element) {
+		return delegate.getImage(element);
+	}
+}
