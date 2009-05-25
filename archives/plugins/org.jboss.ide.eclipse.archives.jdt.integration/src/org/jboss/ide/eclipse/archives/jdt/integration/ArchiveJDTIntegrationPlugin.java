4b6ce8c16668255b3218dc6ad6c084b9de7a88a4
Sean Flanigan <sflaniga@redhat.com>
2009-05-25 04:39:55 +0000

    https://jira.jboss.org/jira/browse/JBIDE-3557 Externalise English Strings
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@15470 a97e2381-89e5-4abb-bab3-167db6db766c

--- archives/plugins/org.jboss.ide.eclipse.archives.jdt.integration/src/org/jboss/ide/eclipse/archives/jdt/integration/ArchiveJDTIntegrationPlugin.java
+++ archives/plugins/org.jboss.ide.eclipse.archives.jdt.integration/src/org/jboss/ide/eclipse/archives/jdt/integration/ArchiveJDTIntegrationPlugin.java
@@ -15,7 +15,7 @@ import org.osgi.framework.BundleContext;
 public class ArchiveJDTIntegrationPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.jboss.ide.eclipse.archives.jdt.integration";
+	public static final String PLUGIN_ID = "org.jboss.ide.eclipse.archives.jdt.integration"; //$NON-NLS-1$
 
 	// The shared instance
 	private static ArchiveJDTIntegrationPlugin plugin;
