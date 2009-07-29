80bfc965066d701a4e97404485811862fc4aa53c
Rob Stryker <rob.stryker@jboss.com>
2009-07-29 19:41:33 +0000

    JBIDE-4680
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@16879 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.archives.webtools/src/org/jboss/ide/eclipse/archives/webtools/filesets/NewServerFilesetHandler.java
+++ as/plugins/org.jboss.ide.eclipse.archives.webtools/src/org/jboss/ide/eclipse/archives/webtools/filesets/NewServerFilesetHandler.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.wst.server.core.IServer;
+import org.jboss.ide.eclipse.as.core.server.IJBossServerRuntime;
 import org.jboss.ide.eclipse.as.core.server.UnitedServerListener;
+import org.jboss.ide.eclipse.as.core.util.ServerConverter;
 
 public class NewServerFilesetHandler extends UnitedServerListener {
 	private static NewServerFilesetHandler instance;
@@ -26,6 +29,23 @@ public class NewServerFilesetHandler extends UnitedServerListener {
 		sets = FilesetUtil.loadFilesets(fileToRead.toFile(), null);
 		list.addAll(Arrays.asList(sets));
 		Fileset[] finalSets = (Fileset[]) list.toArray(new Fileset[list.size()]);
+
+		
+		/*
+		 * Replace {config} with server/config if this is a jboss server
+		 */
+		IJBossServerRuntime ajbsrt = (IJBossServerRuntime) server.getRuntime()
+		.loadAdapter(IJBossServerRuntime.class,
+				new NullProgressMonitor());
+		String config = null;
+		if( ajbsrt != null ) 
+			config = ajbsrt.getJBossConfiguration();
+		for( int i = 0; i < finalSets.length; i++ ) {
+			String folder = finalSets[i].getRawFolder();
+			if( config != null )
+				folder = folder.replace("{config}", "server/" + config); //$NON-NLS-1$ //$NON-NLS-2$
+			finalSets[i].setFolder(folder);
+		}
 		FilesetUtil.saveFilesets(server, finalSets);
 	}
 }
