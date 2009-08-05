d1ed877c5968426c98923d8fbc4a7604ca3f8a73
Rob Stryker <rob.stryker@jboss.com>
2009-08-05 20:24:16 +0000

    Working on superclasses for JBIDE-4320
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@16969 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.wtp.override.core/src/org/jboss/ide/eclipse/as/wtp/override/core/modules/JBTProjectModuleFactory.java
+++ as/plugins/org.jboss.ide.eclipse.as.wtp.override.core/src/org/jboss/ide/eclipse/as/wtp/override/core/modules/JBTProjectModuleFactory.java
@@ -1,10 +1,15 @@
 package org.jboss.ide.eclipse.as.wtp.override.core.modules;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.wst.common.componentcore.ComponentCore;
+import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
+import org.eclipse.wst.common.componentcore.resources.IVirtualReference;
 import org.eclipse.wst.common.project.facet.core.IFacetedProject;
 import org.eclipse.wst.common.project.facet.core.IProjectFacet;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
@@ -69,8 +74,10 @@ public abstract class JBTProjectModuleFactory extends ProjectModuleFactoryDelega
 						moduleType, 
 						version.getVersionString(), 
 						project);
-				moduleToDelegate.put(module, createDelegate(project));
+				JBTProjectModuleDelegate delegate = createDelegate(project);
+				moduleToDelegate.put(module, delegate);
 				
+				createBinaryModules(ComponentCore.createComponent(project), delegate);
 				// TODO - create children!!! see JEEDeployableFactory
 				return new IModule[] { module };
 			}
@@ -79,6 +86,21 @@ public abstract class JBTProjectModuleFactory extends ProjectModuleFactoryDelega
 		}
 		return null;
 	}
+	
+	protected void createBinaryModules(IVirtualComponent component, JBTProjectModuleDelegate delegate) {
+		List projectModules = new ArrayList();
+		IVirtualReference[] references = component.getReferences();
+		for (int i = 0; i < references.length; i++) {
+			IVirtualComponent moduleComponent = references[i].getReferencedComponent();
+			if (moduleComponent.isBinary()) {		
+				if( !delegate.shouldIncludeUtilityComponent(moduleComponent, references, null)) {
+					// if we shouldn't include it there, we should do it here
+					// TODO do this
+				}
+			}
+		}
+	}
+
 	
 	protected abstract JBTProjectModuleDelegate createDelegate(IProject project);
 	
