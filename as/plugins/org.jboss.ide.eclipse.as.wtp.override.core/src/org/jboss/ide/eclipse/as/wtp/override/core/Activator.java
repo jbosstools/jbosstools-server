24acd9f038ea5425f212a0022055c4feb758ae61
Rob Stryker <rob.stryker@jboss.com>
2009-08-07 01:14:22 +0000

    renaming plugin to remove override
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@17001 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.wtp.override.core/src/org/jboss/ide/eclipse/as/wtp/override/core/Activator.java
+++ as/plugins/org.jboss.ide.eclipse.as.wtp.override.core/src/org/jboss/ide/eclipse/as/wtp/override/core/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends Plugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.jboss.ide.eclipse.as.wtp.override.core"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.jboss.ide.eclipse.as.wtp.core"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
