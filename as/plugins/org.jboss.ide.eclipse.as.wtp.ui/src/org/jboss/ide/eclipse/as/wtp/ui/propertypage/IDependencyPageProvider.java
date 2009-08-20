e0f7483b8820102d352ed4f1ece07de6fff3112b
Rob Stryker <rob.stryker@jboss.com>
2009-08-20 02:33:24 +0000

    FUCK SVN
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@17175 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ as/plugins/org.jboss.ide.eclipse.as.wtp.ui/src/org/jboss/ide/eclipse/as/wtp/ui/propertypage/IDependencyPageProvider.java
@@ -0,0 +1,10 @@
+package org.jboss.ide.eclipse.as.wtp.ui.propertypage;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
+
+public interface IDependencyPageProvider {
+	public boolean canHandle(IFacetedProject project);
+	public IModuleDependenciesControl[] createPages(IFacetedProject project, ModuleAssemblyRootPage parent);
+	public Composite createRootControl(IModuleDependenciesControl[] pages, Composite parent);
+}
