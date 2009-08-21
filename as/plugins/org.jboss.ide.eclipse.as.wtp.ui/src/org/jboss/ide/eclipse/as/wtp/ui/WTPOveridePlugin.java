aa34198b330b296fa2670f3ef1bb9cf60ad43853
Rob Stryker <rob.stryker@jboss.com>
2009-08-21 00:33:42 +0000

    ensuring all id's are correct for our temporary page while WTP is slow
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@17196 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.wtp.ui/src/org/jboss/ide/eclipse/as/wtp/ui/WTPOveridePlugin.java
+++ as/plugins/org.jboss.ide.eclipse.as.wtp.ui/src/org/jboss/ide/eclipse/as/wtp/ui/WTPOveridePlugin.java
@@ -13,7 +13,7 @@ import org.osgi.framework.BundleContext;
 public class WTPOveridePlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.jboss.ide.eclipse.as.wtp.override.ui"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.jboss.ide.eclipse.as.wtp.ui"; //$NON-NLS-1$
 
 	// The shared instance
 	private static WTPOveridePlugin plugin;
