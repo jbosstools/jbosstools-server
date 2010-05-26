751bfd16befaa79724a6211aeec0620a720c0855
Denis Golovin <dgolovin@exadel.com>
2010-05-26 18:33:57 +0000

    AllAsuITests renamed to AsUiAllTests to let ticho build find the suite
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@22336 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/tests/org.jboss.ide.eclipse.as.ui.test/src/org/jboss/ide/eclipse/as/ui/test/AsUiAllTests.java
+++ as/tests/org.jboss.ide.eclipse.as.ui.test/src/org/jboss/ide/eclipse/as/ui/test/AsUiAllTests.java
@@ -5,10 +5,10 @@ import org.jboss.ide.eclipse.as.ui.wizards.test.NewServerWizardTest;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-public class AllAsUiTests {
+public class AsUiAllTests {
 	
 	public static Test suite() {
-		TestSuite suite = new TestSuite(AllAsUiTests.class.getName());
+		TestSuite suite = new TestSuite(AsUiAllTests.class.getName());
 		suite.addTestSuite(NewServerWizardTest.class);
 		return suite;
 	}
