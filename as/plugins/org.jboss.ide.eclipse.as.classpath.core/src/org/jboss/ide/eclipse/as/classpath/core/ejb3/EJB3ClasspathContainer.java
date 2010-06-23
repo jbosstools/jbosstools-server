2702d2e819a6d10449be842422fb66702484be97
Rob Stryker <rob.stryker@jboss.com>
2010-06-23 07:23:11 +0000

    JBIDE-6249 - trunk
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@22940 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.classpath.core/src/org/jboss/ide/eclipse/as/classpath/core/ejb3/EJB3ClasspathContainer.java
+++ as/plugins/org.jboss.ide.eclipse.as.classpath.core/src/org/jboss/ide/eclipse/as/classpath/core/ejb3/EJB3ClasspathContainer.java
@@ -11,6 +11,7 @@
 
 package org.jboss.ide.eclipse.as.classpath.core.ejb3;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import org.eclipse.core.runtime.CoreException;
@@ -101,14 +102,18 @@ public class EJB3ClasspathContainer implements IClasspathContainer, ClasspathCon
    }
 
    public IClasspathEntry[] getClasspathEntries() {
-      ArrayList entries = new ArrayList();
-      String id = jbossServer.getServer().getServerType().getRuntimeType().getId();
-      if( id.equals(AS_40)) return get40Jars();
-      if( id.equals(AS_42)) return get42Jars();
-      return (IClasspathEntry[]) entries.toArray(new IClasspathEntry[entries.size()]);
+	   try {
+	      String id = jbossServer.getServer().getServerType().getRuntimeType().getId();
+	      if( id.equals(AS_40)) return get40Jars(homePath, configPath);
+	      if( id.equals(AS_42)) return get42Jars(homePath, configPath);
+	      if( id.equals(AS_50)) return get50Jars(homePath, configPath);
+	      if( id.equals(AS_51)) return get51Jars(homePath, configPath);
+	      if( id.equals(AS_60)) return get60Jars(homePath, configPath);
+	   } catch( FileNotFoundException fnfe ) {}
+	   return new IClasspathEntry[]{};
    }
 
-   public IClasspathEntry[] get40Jars() {
+   protected static IClasspathEntry[] get40Jars(IPath homePath, IPath configPath)  throws FileNotFoundException {
 		ArrayList list = new ArrayList();
 
 		// path roots
@@ -128,9 +133,9 @@ public class EJB3ClasspathContainer implements IClasspathContainer, ClasspathCon
 		// hibernate
 		list.add(getEntry(homePath.append(CLIENT).append(HIBERNATE_CLIENT_JAR)));
 		return (IClasspathEntry[]) list.toArray(new IClasspathEntry[list.size()]);
-   }
-   
-   public IClasspathEntry[] get42Jars() {
+  }
+  
+  protected static IClasspathEntry[] get42Jars(IPath homePath, IPath configPath) throws FileNotFoundException {
 		ArrayList list = new ArrayList();
 
 		// path roots
@@ -153,10 +158,58 @@ public class EJB3ClasspathContainer implements IClasspathContainer, ClasspathCon
 		// persistence jar
 		list.add(getEntry(client.append(EJB3_PERSISTENCE_JAR)));
 		return (IClasspathEntry[]) list.toArray(new IClasspathEntry[list.size()]);
-   }
+  }
    
+   protected static IClasspathEntry[] get50Jars(IPath homePath, IPath configPath)  throws FileNotFoundException {
+		IPath deployers = configPath.append(DEPLOYERS);
+		IPath deployer = deployers.append(EJB3_DEPLOYER);
+		IPath aopDeployer = deployers.append(AOP_JBOSS5_DEPLOYER);
+		IPath client = homePath.append(CLIENT);
+		ArrayList<IClasspathEntry> list = new ArrayList<IClasspathEntry>();
+		list.add(getEntry(aopDeployer.append(JBOSS5_ASPECT_LIBRARY_JAR)));
+		list.add(getEntry(deployer.append(JB5_EJB_DEPLOYER_JAR)));
+		list.add(getEntry(deployer.append(JB5_EJB_IIOP_JAR)));
+		list.add(getEntry(client.append(EJB3_PERSISTENCE_JAR)));
+		list.add(getEntry(client.append(jboss_ejb3_common_client)));
+		list.add(getEntry(client.append(jboss_ejb3_core_client)));
+		list.add(getEntry(client.append(jboss_ejb3_ext_api_impl)));
+		list.add(getEntry(client.append(jboss_ejb3_ext_api)));
+		list.add(getEntry(client.append(jboss_ejb3_proxy_client)));
+		list.add(getEntry(client.append(jboss_ejb3_proxy_clustered_client)));
+		list.add(getEntry(client.append(jboss_ejb3_security_client)));
+		list.add(getEntry(homePath.append(CLIENT).append(JB50_HIBERNATE_ANNOTATIONS_JAR)));
+		return (IClasspathEntry[]) list.toArray(new IClasspathEntry[list.size()]);
+   }
 
-	protected IClasspathEntry getEntry(IPath path) {
+   protected static IClasspathEntry[] get51Jars(IPath homePath, IPath configPath)  throws FileNotFoundException {
+		return get50Jars(homePath, configPath);
+   }
+
+   protected static IClasspathEntry[] get60Jars(IPath homePath, IPath configPath)  throws FileNotFoundException {
+		IPath deployers = configPath.append(DEPLOYERS);
+		IPath aopDeployer = deployers.append(AOP_JBOSS5_DEPLOYER);
+		IPath client = homePath.append(CLIENT);
+		ArrayList<IClasspathEntry> list = new ArrayList<IClasspathEntry>();
+		list.add(getEntry(aopDeployer.append(JBOSS6_AOP_ASPECTS_JAR)));
+		list.add(getEntry(aopDeployer.append(JBOSS6_AS_ASPECT_LIBRARY_JAR)));
+		list.add(getEntry(deployers.append(JB6_EJB3_ENDPOINT_DEPLOYER_JAR)));
+		list.add(getEntry(deployers.append(JB6_EJB3_METRICS_DEPLOYER_JAR)));
+		list.add(getEntry(client.append(jboss_ejb3_common_client)));
+		list.add(getEntry(client.append(jboss_ejb3_core_client)));
+		list.add(getEntry(client.append(jboss_ejb3_ext_api_impl)));
+		list.add(getEntry(client.append(jboss_ejb3_ext_api)));
+		list.add(getEntry(client.append(jboss6_ejb3_proxy_spi_client)));
+		list.add(getEntry(client.append(jboss6_ejb3_proxy_impl_client)));
+		list.add(getEntry(client.append(jboss_ejb3_proxy_clustered_client)));
+		list.add(getEntry(client.append(jboss_ejb3_security_client)));
+		list.add(getEntry(homePath.append(CLIENT).append(JB50_HIBERNATE_ANNOTATIONS_JAR)));
+		return (IClasspathEntry[]) list.toArray(new IClasspathEntry[list.size()]);
+   }   
+
+	protected static IClasspathEntry getEntry(IPath path) throws FileNotFoundException {
+		System.out.println("ls " + path.toOSString().substring(45));
+		if( !path.toFile().exists())
+			throw new FileNotFoundException();
 		return JavaRuntime.newArchiveRuntimeClasspathEntry(path).getClasspathEntry();
 	}
 
