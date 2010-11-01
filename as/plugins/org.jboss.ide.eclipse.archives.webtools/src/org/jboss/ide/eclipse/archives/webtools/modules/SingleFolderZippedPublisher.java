3f3c4e153d8b5d8def0d65da12498a49e0614ad9
Rob Stryker <rob.stryker@jboss.com>
2010-11-01 11:15:38 +0000

    JBIDE-7471 - zipped folder publishing order
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@26154 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.archives.webtools/src/org/jboss/ide/eclipse/archives/webtools/modules/SingleFolderZippedPublisher.java
+++ as/plugins/org.jboss.ide.eclipse.archives.webtools/src/org/jboss/ide/eclipse/archives/webtools/modules/SingleFolderZippedPublisher.java
@@ -16,16 +16,19 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.wst.server.core.IModule;
 import org.eclipse.wst.server.core.IServer;
 import org.jboss.ide.eclipse.as.core.modules.SingleDeployableFactory;
+import org.jboss.ide.eclipse.as.core.server.IDeployableServer;
+import org.jboss.ide.eclipse.as.core.util.ServerConverter;
 import org.jboss.ide.eclipse.as.wtp.core.modules.IJBTModule;
 
 public class SingleFolderZippedPublisher extends WTPZippedPublisher {
 	public boolean accepts(String method, IServer server, IModule[] module) {
+		IDeployableServer ds = ServerConverter.getDeployableServer(server);
 		if( module != null && module.length > 0 
 				&& module[module.length-1] != null  
 				&& module[module.length-1].getModuleType().getId().equals(SingleDeployableFactory.MODULE_TYPE)) {
 			IModule mod = module[module.length-1];
 			IJBTModule del = (IJBTModule)mod.loadAdapter(IJBTModule.class, new NullProgressMonitor());
-			if( del != null && !del.isBinary())
+			if( del != null && !del.isBinary() && ds.zipsWTPDeployments())
 				return true; // we have a folder
 		}
 		return false;
--- as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/publishers/SingleFilePublisher.java
+++ as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/publishers/SingleFilePublisher.java
@@ -14,17 +14,23 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.wst.server.core.IModule;
 import org.eclipse.wst.server.core.IServer;
 import org.jboss.ide.eclipse.as.core.modules.SingleDeployableFactory;
+import org.jboss.ide.eclipse.as.core.server.IDeployableServer;
+import org.jboss.ide.eclipse.as.core.util.ServerConverter;
 import org.jboss.ide.eclipse.as.wtp.core.modules.IJBTModule;
 
 public class SingleFilePublisher extends AbstractServerToolsPublisher {
 	public boolean accepts(String method, IServer server, IModule[] module) {
+		IDeployableServer ds = ServerConverter.getDeployableServer(server);
 		if( module != null && module.length > 0 
 				&& module[module.length-1] != null  
 				&& module[module.length-1].getModuleType().getId().equals(SingleDeployableFactory.MODULE_TYPE)) {
 			IModule mod = module[module.length-1];
 			IJBTModule del = (IJBTModule)mod.loadAdapter(IJBTModule.class, new NullProgressMonitor());
-			if( del != null && del.isBinary())
-				return true;
+			//if( del != null && del.isBinary())
+			if( del != null ) {
+				if( del.isBinary() || !ds.zipsWTPDeployments())
+					return true;
+			}
 		}
 		return false;
 	}
