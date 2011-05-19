2b933ac98d54edd2d168e0a744db84909ce19d8e
Rob Stryker <rob.stryker@jboss.com>
2011-05-19 13:34:14 +0000

    JBIDE-8960 - removal of deprecated code
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@31377 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/publishers/UnifiedPublisher.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.ide.eclipse.as.core.publishers;
-
-import org.jboss.ide.eclipse.as.core.server.internal.v7.JBoss7JSTPublisher;
-
-public class UnifiedPublisher extends JBoss7JSTPublisher {
-
-}
