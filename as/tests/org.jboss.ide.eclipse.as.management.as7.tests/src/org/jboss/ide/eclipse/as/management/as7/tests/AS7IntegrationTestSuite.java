553f90adcde3067ac48b0b955485d8081ec5e7f8
Andre Dietisheim <adietish@redhat.com>
2011-06-14 20:02:47 +0000

    [JBIDE-9137] added integration tests suite
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@32095 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ as/tests/org.jboss.ide.eclipse.as.management.as7.tests/src/org/jboss/ide/eclipse/as/management/as7/tests/AS7IntegrationTestSuite.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.ide.eclipse.as.management.as7.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author Andre Dietisheim
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	AS7ManagerIntegrationTest.class
+	, JBossManagementServiceIntegrationTest.class
+})
+public class AS7IntegrationTestSuite {
+
+}
