2b8f201bf8496d3a5a0dd24ae8d8dc6bc08d027d
Andre Dietisheim <adietish@redhat.com>
2011-06-14 20:22:34 +0000

    [JBIDE-9137] added pom, renamed packages to internal packages
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@32096 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/tests/org.jboss.ide.eclipse.as.management.as7.tests/src/org/jboss/ide/eclipse/as/internal/management/as7/tests/AS7IntegrationTestSuite.java
+++ as/tests/org.jboss.ide.eclipse.as.management.as7.tests/src/org/jboss/ide/eclipse/as/internal/management/as7/tests/AS7IntegrationTestSuite.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Red Hat, Inc. - initial API and implementation
  ******************************************************************************/
-package org.jboss.ide.eclipse.as.management.as7.tests;
+package org.jboss.ide.eclipse.as.internal.management.as7.tests;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
--- as/tests/org.jboss.ide.eclipse.as.management.as7.tests/src/org/jboss/ide/eclipse/as/internal/management/as7/tests/JBossManagementServiceIntegrationTest.java
+++ as/tests/org.jboss.ide.eclipse.as.management.as7.tests/src/org/jboss/ide/eclipse/as/internal/management/as7/tests/JBossManagementServiceIntegrationTest.java
@@ -8,7 +8,7 @@
  * Contributors: 
  * Red Hat, Inc. - initial API and implementation 
  ******************************************************************************/
-package org.jboss.ide.eclipse.as.management.as7.tests;
+package org.jboss.ide.eclipse.as.internal.management.as7.tests;
 
 import static org.junit.Assert.assertNotNull;
 
