42a65bf8d011c36ec2025b762d0eb9363335f766
Andre Dietisheim <adietish@redhat.com>
2011-06-24 10:49:20 +0000

    [JBIDE-9247] added copyright headers where missing in the tests
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@32347 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/tests/org.jboss.ide.eclipse.as.test/src/org/jboss/ide/eclipse/as/test/publishing/v2/MockJSTPublisherTestDynUtil.java
+++ as/tests/org.jboss.ide.eclipse.as.test/src/org/jboss/ide/eclipse/as/test/publishing/v2/MockJSTPublisherTestDynUtil.java
@@ -1,5 +1,14 @@
 package org.jboss.ide.eclipse.as.test.publishing.v2;
-
+/******************************************************************************* 
+ * Copyright (c) 2011 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
 import java.io.IOException;
 import java.util.Arrays;
 
