453fecf329800475bc0fb1777a8a6187bc916ae6
Andre Dietisheim <adietish@redhat.com>
2011-06-29 10:19:34 +0000

    [JBIDE-9225] removed ASPerspecitveTest from the test suite, marked the test as deprecated and commented its methods
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@32431 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/tests/org.jboss.ide.eclipse.as.ui.test/src/org/jboss/ide/eclipse/as/ui/perspective/test/ASPerspectiveTest.java
+++ as/tests/org.jboss.ide.eclipse.as.ui.test/src/org/jboss/ide/eclipse/as/ui/perspective/test/ASPerspectiveTest.java
@@ -2,25 +2,31 @@ package org.jboss.ide.eclipse.as.ui.perspective.test;
 
 import junit.framework.TestCase;
 
+import org.eclipse.ui.IPerspectiveDescriptor;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.jboss.ide.eclipse.as.ui.perspective.ASPerspective;
+import org.jboss.ide.eclipse.as.ui.views.ServerLogView;
+
 @Deprecated
 public class ASPerspectiveTest extends TestCase {
 
-//	@Override
-//	protected void setUp() throws Exception {
-//		PlatformUI.getWorkbench()
-//		.getActiveWorkbenchWindow().getActivePage().setPerspective(
-//				PlatformUI.getWorkbench().getPerspectiveRegistry().findPerspectiveWithId(ASPerspective.ID));
-//	}
+	@Override
+	protected void setUp() throws Exception {
+		PlatformUI.getWorkbench()
+		.getActiveWorkbenchWindow().getActivePage().setPerspective(
+				PlatformUI.getWorkbench().getPerspectiveRegistry().findPerspectiveWithId(ASPerspective.ID));
+	}
 
-//	public void testASPerspective() {
-//		IPerspectiveDescriptor perspective = PlatformUI.getWorkbench()
-//		.getActiveWorkbenchWindow().getActivePage().getPerspective();
-//		assertEquals(perspective.getLabel(),"JBoss AS");
-//	}
-//
-//	public void testServerLogView() throws PartInitException {
-//		PlatformUI.getWorkbench().getActiveWorkbenchWindow()
-//			.getActivePage().showView(ServerLogView.VIEW_ID);
-//	}
+	public void testASPerspective() {
+		IPerspectiveDescriptor perspective = PlatformUI.getWorkbench()
+		.getActiveWorkbenchWindow().getActivePage().getPerspective();
+		assertEquals(perspective.getLabel(),"JBoss AS");
+	}
+
+	public void testServerLogView() throws PartInitException {
+		PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+			.getActivePage().showView(ServerLogView.VIEW_ID);
+	}
 
 }
