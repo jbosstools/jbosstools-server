daefd7b4ef782a336e2359d2d07ece6f826da165
Andre Dietisheim <adietish@redhat.com>
2011-07-06 16:08:18 +0000

    [JBIDE-9215] removed unneeded empty publishing methods
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@32661 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/AbstractJBossBehaviourDelegate.java
+++ as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/AbstractJBossBehaviourDelegate.java
@@ -11,7 +11,6 @@
 package org.jboss.ide.eclipse.as.core.server.internal;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.wst.server.core.IServer;
@@ -42,14 +41,6 @@ public abstract class AbstractJBossBehaviourDelegate implements JBossBehaviourDe
 
 	protected abstract IStatus gracefullStop();
 	
-	@Override
-	public void publishStart(IProgressMonitor monitor) throws CoreException {
-	}
-
-	@Override
-	public void publishFinish(IProgressMonitor monitor) throws CoreException {
-	}
-
 	@Deprecated
 	public void serverStarting() {
 		setServerStarting();
