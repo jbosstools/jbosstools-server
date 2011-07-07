b1041ed079a8d04af2f632533f096c844c11ddac
Andre Dietisheim <adietish@redhat.com>
2011-07-07 12:35:52 +0000

    [JBIDE-9215] added copyright header, reformatted code to be better readable
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@32700 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/launch/IStartLaunchDelegate.java
+++ as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/launch/IStartLaunchDelegate.java
@@ -1,3 +1,13 @@
+/******************************************************************************* 
+ * Copyright (c) 2007 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
 package org.jboss.ide.eclipse.as.core.server.internal.launch;
 
 import org.eclipse.core.runtime.CoreException;
@@ -6,13 +16,16 @@ import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 
 public interface IStartLaunchDelegate {
-	public void actualLaunch(DelegatingStartLaunchConfiguration launchConfig,
-			ILaunchConfiguration configuration, 
+	
+	public void actualLaunch(DelegatingStartLaunchConfiguration launchConfig, ILaunchConfiguration configuration, 
 			String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException;
 	
-	public boolean preLaunchCheck(ILaunchConfiguration configuration, String mode, IProgressMonitor monitor) throws CoreException;
-	public void preLaunch(ILaunchConfiguration configuration, 
-			String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException;
+	public boolean preLaunchCheck(ILaunchConfiguration configuration, String mode, IProgressMonitor monitor) 
+			throws CoreException;
+
+	public void preLaunch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) 
+			throws CoreException;
+
 	public void postLaunch(ILaunchConfiguration configuration, String mode,
 			ILaunch launch, IProgressMonitor monitor) throws CoreException;
 	
