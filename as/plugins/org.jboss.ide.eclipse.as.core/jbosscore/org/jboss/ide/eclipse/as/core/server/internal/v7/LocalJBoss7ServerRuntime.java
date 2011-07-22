2a4f86ed762be17b3345f8ead523365628e85d8e
Andre Dietisheim <adietish@redhat.com>
2011-07-22 08:56:52 +0000

    [jbide-9391] reformatted argument creation so that each argument is on its own line (and better readable to catch mistakes, removals, etc.)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@33131 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/v7/LocalJBoss7ServerRuntime.java
+++ as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/v7/LocalJBoss7ServerRuntime.java
@@ -29,23 +29,27 @@ public class LocalJBoss7ServerRuntime extends LocalJBossServerRuntime {
 
 	@Override
 	public String getDefaultRunArgs(IPath serverHome) {
-		return "-mp \"" //$NON-NLS-1$
-				+ serverHome.append("modules").toString() //$NON-NLS-1$ 
-				+ "\" -logmodule org.jboss.logmanager -jaxpmodule javax.xml.jaxp-provider org.jboss.as.standalone"; //$NON-NLS-1$
+		return "-mp \"" + serverHome.append("modules").toString() + "\"" //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
+				+ " -logmodule org.jboss.logmanager" //$NON-NLS-1$
+				+ " -jaxpmodule javax.xml.jaxp-provider" //$NON-NLS-1$
+				+ " org.jboss.as.standalone"; //$NON-NLS-1$
 	}
 		
 	@Override
 	public String getDefaultRunVMArgs(IPath serverHome) {
 		IPath bootLog = serverHome.append("standalone").append("log").append("boot.log"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		IPath logConfig = serverHome.append("standalone").append("configuration").append("logging.properties"); //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
-		return "-server -Xms64m -Xmx512m -XX:MaxPermSize=256m " //$NON-NLS-1$
-				+ "-Djava.net.preferIPv4Stack=true " //$NON-NLS-1$
-				+ "-Dorg.jboss.resolver.warning=true "  //$NON-NLS-1$
-				+ "-Dsun.rmi.dgc.client.gcInterval=3600000 " //$NON-NLS-1$
-				+ "-Dsun.rmi.dgc.server.gcInterval=3600000 " //$NON-NLS-1$
-				+ "\"-Dorg.jboss.boot.log.file=" + bootLog.toString() + "\" " //$NON-NLS-1$ //$NON-NLS-2$
-				+ "\"-Dlogging.configuration=file:" + logConfig.toString() + "\" " //$NON-NLS-1$ //$NON-NLS-2$ 
-				+ "\"-Djboss.home.dir=" + serverHome.toString() + "\""; //$NON-NLS-1$ //$NON-NLS-2$"
+		return "-server" //$NON-NLS-1$
+				+ " -Xms64m" //$NON-NLS-1$
+				+ " -Xmx512m" //$NON-NLS-1$
+				+ " -XX:MaxPermSize=256m" //$NON-NLS-1$
+				+ " -Djava.net.preferIPv4Stack=true" //$NON-NLS-1$
+				+ " -Dorg.jboss.resolver.warning=true"  //$NON-NLS-1$
+				+ " -Dsun.rmi.dgc.client.gcInterval=3600000" //$NON-NLS-1$
+				+ " -Dsun.rmi.dgc.server.gcInterval=3600000" //$NON-NLS-1$
+				+ " \"-Dorg.jboss.boot.log.file=" + bootLog.toString() + "\"" //$NON-NLS-1$ //$NON-NLS-2$
+				+ " \"-Dlogging.configuration=file:" + logConfig.toString() + "\"" //$NON-NLS-1$ //$NON-NLS-2$ 
+				+ " \"-Djboss.home.dir=" + serverHome.toString() + "\""; //$NON-NLS-1$ //$NON-NLS-2$"
 	}
 
 	@Override
@@ -53,13 +57,16 @@ public class LocalJBoss7ServerRuntime extends LocalJBossServerRuntime {
 		IPath loc = getRuntime().getLocation();
 		IPath bootLog = loc.append("standalone").append("log").append("boot.log"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		IPath logConfig = loc.append("standalone").append("configuration").append("logging.properties"); //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
-		return "-server -Xms64m -Xmx512m -XX:MaxPermSize=256m " //$NON-NLS-1$
-				+ "-Djava.net.preferIPv4Stack=true " //$NON-NLS-1$
-				+ "-Dorg.jboss.resolver.warning=true "  //$NON-NLS-1$
-				+ "-Dsun.rmi.dgc.client.gcInterval=3600000 " //$NON-NLS-1$
-				+ "-Dsun.rmi.dgc.server.gcInterval=3600000 " //$NON-NLS-1$
-				+ "\"-Dorg.jboss.boot.log.file=" + bootLog.toString() + "\" " //$NON-NLS-1$ //$NON-NLS-2$
-				+ "\"-Dlogging.configuration=file:" + logConfig.toString() + "\" " //$NON-NLS-1$ //$NON-NLS-2$ 
-				+ "\"-Djboss.home.dir=" + loc.toString() + "\""; //$NON-NLS-1$ //$NON-NLS-2$"
+		return "-server" //$NON-NLS-1$
+				+ " -Xms64m" //$NON-NLS-1$
+				+ " -Xmx512m" //$NON-NLS-1$
+				+ " -XX:MaxPermSize=256m" //$NON-NLS-1$
+				+ " -Djava.net.preferIPv4Stack=true" //$NON-NLS-1$
+				+ " -Dorg.jboss.resolver.warning=true"  //$NON-NLS-1$
+				+ " -Dsun.rmi.dgc.client.gcInterval=3600000" //$NON-NLS-1$
+				+ " -Dsun.rmi.dgc.server.gcInterval=3600000" //$NON-NLS-1$
+				+ " \"-Dorg.jboss.boot.log.file=" + bootLog.toString() + "\"" //$NON-NLS-1$ //$NON-NLS-2$
+				+ " \"-Dlogging.configuration=file:" + logConfig.toString() + "\"" //$NON-NLS-1$ //$NON-NLS-2$ 
+				+ " \"-Djboss.home.dir=" + loc.toString() + "\""; //$NON-NLS-1$ //$NON-NLS-2$"
 	}
 }
