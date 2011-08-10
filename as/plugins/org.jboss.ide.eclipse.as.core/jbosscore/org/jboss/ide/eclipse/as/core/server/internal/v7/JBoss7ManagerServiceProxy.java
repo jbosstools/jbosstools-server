61b9ec01a1923edec30ae67fca4cfbfcf236f1ed
Andre Dietisheim <adietish@redhat.com>
2011-08-10 09:59:47 +0000

    added copyright header and author comment
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@33758 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/v7/JBoss7ManagerServiceProxy.java
+++ as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/v7/JBoss7ManagerServiceProxy.java
@@ -1,3 +1,13 @@
+/******************************************************************************* 
+ * Copyright (c) 2007 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
 package org.jboss.ide.eclipse.as.core.server.internal.v7;
 
 import java.io.File;
@@ -9,6 +19,9 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.util.tracker.ServiceTracker;
 
+/**
+ * @author André Dietisheim
+ */
 public class JBoss7ManagerServiceProxy extends ServiceTracker<IJBoss7ManagerService, IJBoss7ManagerService>
 		implements IJBoss7ManagerService {
 
