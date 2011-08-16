5b5026cd8d9adcd1190648a8b2e585ac30e06f7e
Andre Dietisheim <adietish@redhat.com>
2011-08-16 08:05:18 +0000

    [JBIDE-9511] implemented commit and push handler
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@33960 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.egit.ui/src/org/jboss/ide/eclipse/as/egit/internal/ui/EGitUIActivator.java
+++ as/plugins/org.jboss.ide.eclipse.as.egit.ui/src/org/jboss/ide/eclipse/as/egit/internal/ui/EGitUIActivator.java
@@ -1,4 +1,4 @@
-package org.jboss.ide.eclipse.as.egit.ui;
+package org.jboss.ide.eclipse.as.egit.internal.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -6,18 +6,18 @@ import org.osgi.framework.BundleContext;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class EGitUIActivator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.jboss.ide.eclipse.as.egit.ui"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.jboss.ide.eclipse.as.egit.internal.ui"; //$NON-NLS-1$
 
 	// The shared instance
-	private static Activator plugin;
+	private static EGitUIActivator plugin;
 	
 	/**
 	 * The constructor
 	 */
-	public Activator() {
+	public EGitUIActivator() {
 	}
 
 	/*
@@ -43,7 +43,7 @@ public class Activator extends AbstractUIPlugin {
 	 *
 	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public static EGitUIActivator getDefault() {
 		return plugin;
 	}
 
