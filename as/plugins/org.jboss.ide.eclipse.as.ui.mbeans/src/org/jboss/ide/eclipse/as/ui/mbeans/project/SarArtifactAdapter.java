b71f384ab75fe5870627aecd4de9e654aa940dd8
Rob Stryker <rob.stryker@jboss.com>
2011-08-30 11:06:01 +0000

    /JBIDE-9497 - missing piece regarding the run-on-server (needed an artifact adapter)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@34383 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ as/plugins/org.jboss.ide.eclipse.as.ui.mbeans/src/org/jboss/ide/eclipse/as/ui/mbeans/project/SarArtifactAdapter.java
@@ -0,0 +1,40 @@
+package org.jboss.ide.eclipse.as.ui.mbeans.project;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.wst.server.core.IModule;
+import org.eclipse.wst.server.core.IModuleArtifact;
+import org.eclipse.wst.server.core.ServerUtil;
+import org.eclipse.wst.server.core.model.ModuleArtifactAdapterDelegate;
+
+public class SarArtifactAdapter extends ModuleArtifactAdapterDelegate {
+	public IModuleArtifact getModuleArtifact(Object obj) {
+		if( obj instanceof IJavaElement ) {
+			obj = ((IJavaElement)obj).getJavaProject().getProject();
+		}
+		if( obj instanceof IResource ) {
+			IProject p = ((IResource)obj).getProject();
+			if( p != null ) {
+				IModule[] mods = ServerUtil.getModules(p);
+				if( mods.length == 1 ) {
+					return new MBeanNullArtifact(mods[0]);
+				}
+			}
+		}
+		return null;
+	}
+	
+	public static class MBeanNullArtifact implements IModuleArtifact {
+		private IModule module;
+		public MBeanNullArtifact(IModule mod) {
+			this.module = mod;
+		}
+		
+		@Override
+		public IModule getModule() {
+			return module;
+		}
+	}
+
+}
