605bab662434dc0c4261e84f3176c31e5c0cf219
Andre Dietisheim <adietish@redhat.com>
2011-10-06 19:31:29 +0000

    [JBIDE-9857] corrected plug-in id so that we send the correct user-agent
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@35432 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/OpenshiftUIActivator.java
+++ as/plugins/org.jboss.tools.openshift.express.ui/src/org/jboss/tools/openshift/express/internal/ui/OpenshiftUIActivator.java
@@ -11,7 +11,7 @@ import org.osgi.framework.BundleContext;
 public class OpenshiftUIActivator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.jboss.ide.eclipse.as.openshift.ui"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.jboss.tools.openshift.express.ui"; //$NON-NLS-1$
 
 	// The shared instance
 	private static OpenshiftUIActivator plugin;
