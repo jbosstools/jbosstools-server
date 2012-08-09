9911e51eed786690d8e2d0b0838e60d921be7eef
Rob Stryker <rob.stryker@jboss.com>
2012-08-09 04:32:47 +0000

    JBIDE-12403 to maintenance
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.3.x@42940 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/launch/configuration/JBossLaunchConfigProperties.java
+++ as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/server/internal/launch/configuration/JBossLaunchConfigProperties.java
@@ -180,11 +180,11 @@ public class JBossLaunchConfigProperties {
 
 	public void setServerFlag(boolean useFlag, IJBossServerRuntime runtime,
 			ILaunchConfigurationWorkingCopy launchConfig) throws CoreException {
-		String programArguments = getProgramArguments(launchConfig);
+		String programArguments = getVMArguments(launchConfig);
 		String newArgs = useFlag ? 
 				ArgsUtil.setFlag(programArguments, IJBossRuntimeConstants.SERVER_ARG) :
 				ArgsUtil.clearFlag(programArguments, IJBossRuntimeConstants.SERVER_ARG);
-		setProgramArguments(newArgs, launchConfig);
+		setVmArguments(newArgs, launchConfig);
 	}
 
 	public boolean isServerHomeSet(ILaunchConfigurationWorkingCopy launchConfig) throws CoreException {
