ebc3f9b24ffcbc3a3b78b54b019f36c74937c329
Rob Stryker <rob@oxbeef.net>
2013-01-08 15:14:31 +0800

    sar virtual component can return a more specific subclass

--- as/plugins/org.jboss.ide.eclipse.as.ui.mbeans/src/org/jboss/ide/eclipse/as/ui/mbeans/project/SARVirtualComponent.java
+++ as/plugins/org.jboss.ide.eclipse.as.ui.mbeans/src/org/jboss/ide/eclipse/as/ui/mbeans/project/SARVirtualComponent.java
@@ -1,9 +1,22 @@
 package org.jboss.ide.eclipse.as.ui.mbeans.project;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.wst.common.componentcore.internal.util.IComponentImplFactory;
+import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
 import org.jboss.ide.eclipse.as.wtp.core.vcf.JBTVirtualComponent;
 
 public class SARVirtualComponent extends JBTVirtualComponent implements
 		IComponentImplFactory {
+	public SARVirtualComponent() {
+		super();
+	}
+	
+	public SARVirtualComponent(IProject aProject, Path path) {
+		super(aProject, path);
+	}
 
+	public IVirtualComponent createComponent(IProject aProject) {
+		return new SARVirtualComponent(aProject, new Path("/")); //$NON-NLS-1$
+	}
 }
