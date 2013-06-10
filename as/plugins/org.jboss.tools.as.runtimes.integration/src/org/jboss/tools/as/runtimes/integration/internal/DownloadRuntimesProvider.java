54cdec050a65e081899305abe0f8466e1039e1ad
Rob Stryker <rob@oxbeef.net>
2013-06-10 18:56:06 +0800

    JBIDE-14413

--- as/plugins/org.jboss.tools.as.runtimes.integration/src/org/jboss/tools/as/runtimes/integration/internal/DownloadRuntimesProvider.java
+++ as/plugins/org.jboss.tools.as.runtimes.integration/src/org/jboss/tools/as/runtimes/integration/internal/DownloadRuntimesProvider.java
@@ -36,8 +36,8 @@ public class DownloadRuntimesProvider implements IDownloadRuntimesProvider {
 	public DownloadRuntimesProvider() {
 	}
 
-	private Stacks getStacks(IProgressMonitor monitor) {
-		return getStacksManager().getStacks(monitor);
+	private Stacks[] getStacks(IProgressMonitor monitor) {
+		return new StacksManager().getStacks("Loading Downloadable Runtimes", monitor, StacksManager.StacksType.PRESTACKS_TYPE, StacksManager.StacksType.STACKS_TYPE);
 	}
 	
 	private ArrayList<DownloadRuntime> downloads = null;
@@ -51,49 +51,53 @@ public class DownloadRuntimesProvider implements IDownloadRuntimesProvider {
 	
 	private synchronized void loadDownloadableRuntimes(IProgressMonitor monitor) {
 		monitor.beginTask(Messages.LoadRemoteRuntimes, 200);
-		Stacks stacks = getStacks(new SubProgressMonitor(monitor, 100));
-		if( stacks != null ) {
-			ArrayList<DownloadRuntime> tmp = new ArrayList<DownloadRuntime>();
-
-			List<org.jboss.jdf.stacks.model.Runtime> runtimes = stacks.getAvailableRuntimes();
-			Iterator<org.jboss.jdf.stacks.model.Runtime> i = runtimes.iterator();
-			org.jboss.jdf.stacks.model.Runtime workingRT = null;
-			IProgressMonitor creationMonitor = new SubProgressMonitor(monitor, 100);
-			creationMonitor.beginTask(Messages.CreateDownloadRuntimes, runtimes.size() * 100);
-			while(i.hasNext()) {
-				workingRT = i.next();
-				String wtpRT = workingRT.getLabels().getProperty(LABEL_WTP_RUNTIME);
-				String url = workingRT.getDownloadUrl();
-				if( wtpRT != null && url != null && !"".equals(url)) {
-					// We can make a DL out of this
-					String fileSize = workingRT.getLabels().getProperty(LABEL_FILE_SIZE);
-					String license = workingRT.getLicense();
-					String id = workingRT.getId();
-					String legacyId = getLegacyId(id);
-					String effectiveId = legacyId == null ? id : legacyId;
-					
-					String name = workingRT.getName();
-					String version = workingRT.getVersion();
-					DownloadRuntime dr = new DownloadRuntime(effectiveId, name, version, url);
-					dr.setLicenseURL(license);
-					dr.setSize(fileSize);
-					dr.setProperty(PROP_WTP_RUNTIME, wtpRT);
-					if( legacyId != null )
-						dr.setProperty(DownloadRuntime.PROPERTY_ALTERNATE_ID, id);
-					tmp.add(dr);
-				}
-				creationMonitor.worked(100);
+		Stacks[] stacksArr = getStacks(new SubProgressMonitor(monitor, 100));
+		ArrayList<DownloadRuntime> all = new ArrayList<DownloadRuntime>();
+		monitor.beginTask(Messages.CreateDownloadRuntimes, stacksArr.length * 100);		
+		for( int i = 0; i < stacksArr.length; i++ ) {
+			if( stacksArr[i] != null ) {
+				traverseStacks(stacksArr[i], all, new SubProgressMonitor(monitor, 100));
 			}
-			creationMonitor.done();
-			monitor.done();
-			downloads = tmp;
+		}
+		monitor.done();
+		downloads = all;
+	}
+	
+	private void traverseStacks(Stacks stacks, ArrayList<DownloadRuntime> list, IProgressMonitor monitor) {
+		List<org.jboss.jdf.stacks.model.Runtime> runtimes = stacks.getAvailableRuntimes();
+		Iterator<org.jboss.jdf.stacks.model.Runtime> i = runtimes.iterator();
+		org.jboss.jdf.stacks.model.Runtime workingRT = null;
+		monitor.beginTask(Messages.CreateDownloadRuntimes, runtimes.size() * 100);
+		while(i.hasNext()) {
+			workingRT = i.next();
+			String wtpRT = workingRT.getLabels().getProperty(LABEL_WTP_RUNTIME);
+			String url = workingRT.getDownloadUrl();
+			if( wtpRT != null && url != null && !"".equals(url)) {
+				// We can make a DL out of this
+				String fileSize = workingRT.getLabels().getProperty(LABEL_FILE_SIZE);
+				String license = workingRT.getLicense();
+				String id = workingRT.getId();
+				String legacyId = getLegacyId(id);
+				String effectiveId = legacyId == null ? id : legacyId;
+				
+				String name = workingRT.getName();
+				String version = workingRT.getVersion();
+				DownloadRuntime dr = new DownloadRuntime(effectiveId, name, version, url);
+				dr.setLicenseURL(license);
+				dr.setSize(fileSize);
+				dr.setProperty(PROP_WTP_RUNTIME, wtpRT);
+				if( legacyId != null )
+					dr.setProperty(DownloadRuntime.PROPERTY_ALTERNATE_ID, id);
+				list.add(dr);
+			}
+			monitor.worked(100);
 		}
 	}
 	
+	
 	private HashMap<String, String> LEGACY_HASHMAP = null;
 	
-	// Given a stacks.yaml runtime id, get the legacy 
-	// downloadRuntimes id that's required
+	// Given a stacks.yaml runtime id, get the legacy downloadRuntimes id that's required
 	private synchronized String getLegacyId(String id) {
 		if( LEGACY_HASHMAP == null )
 			loadLegacy();
@@ -113,20 +117,4 @@ public class DownloadRuntimesProvider implements IDownloadRuntimesProvider {
 		LEGACY_HASHMAP.put("jboss-as710runtime", "org.jboss.tools.runtime.core.as.710" );
 		LEGACY_HASHMAP.put("jboss-as711runtime", "org.jboss.tools.runtime.core.as.711" );
 	}
-	
-	
-	// Let's pull from my topic branch for now
-	// This is almost no different than us pulling from my hard-coded plugin.xml. 
-	private static final String STACKS_YAML_TOPIC_BRANCH_URL = "https://raw.github.com/robstryker/jdf-stack/e81a884195be83d063c9bb147d1b7bf4fb333e7d/stacks.yaml";
-	
-	// return a custom stacks manager, until our data is upstream in stacks.yaml
-	private StacksManager getStacksManager() {
-		// TODO LATER just return the ORIGINAL stacks manager, once we have a coherent stacks decision!
-		return new StacksManager() {
-			public Stacks getStacks(IProgressMonitor monitor) {
-				return getStacks(STACKS_YAML_TOPIC_BRANCH_URL, "stacks", "yaml", monitor);
-			}
-		};
-	}
-
 }
