ed336e9f8b4d9c4163658ed5451fb3d77905c507
Rob Stryker <rob@oxbeef.net>
2013-07-31 18:17:30 +0800

    JBIDE-15310 - explicit detection for jpp6.1
    uncommenting test suite classes (used during testing)
    
    JBIDE-15310 forgot to update tests
    
    Adding override tag

---
+++ as/plugins/org.jboss.ide.eclipse.as.wtp.core/src/org/jboss/ide/eclipse/as/core/server/bean/ServerBeanTypeJPP61.java
@@ -0,0 +1,34 @@
+/******************************************************************************* 
+ * Copyright (c) 2013 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.jboss.ide.eclipse.as.core.server.bean;
+
+import java.io.File;
+
+import org.jboss.ide.eclipse.as.core.server.bean.ServerBeanTypeUnknownAS72Product.UnknownAS72ProductServerTypeCondition;
+
+public class ServerBeanTypeJPP61 extends ServerBeanTypeUnknownAS71Product {
+	public ServerBeanTypeJPP61() {
+		super("JPP", "JBoss Portal Platform", 
+				asPath("modules","system","layers","base","org","jboss","as","server","main"),
+				new String[] { V6_1 },
+				new JPP61Condition());
+	}
+	public static class JPP61Condition extends UnknownAS72ProductServerTypeCondition {
+		@Override
+		public boolean isServerRoot(File location) {
+			if( "JPP".equalsIgnoreCase(getSlot(location))) {
+				String v = getFullVersion(location, null);
+				return v != null && v.startsWith(V6_1);
+			}
+			return false;
+		}
+	}
+}
