d1dadcd3ddbb37ff0c359d33ce658de11d17e4a5
Rob Stryker <rob@oxbeef.net>
2013-08-16 14:08:49 +0800

    JBIDE-15343

--- as/plugins/org.jboss.ide.eclipse.as.rse.core/src/org/jboss/ide/eclipse/as/rse/core/RSEJBoss7BehaviourDelegate.java
+++ as/plugins/org.jboss.ide.eclipse.as.rse.core/src/org/jboss/ide/eclipse/as/rse/core/RSEJBoss7BehaviourDelegate.java
@@ -26,6 +26,10 @@ import org.jboss.ide.eclipse.as.core.server.v7.management.AS7ManagementDetails;
 import org.jboss.ide.eclipse.as.management.core.IJBoss7ManagerService;
 import org.jboss.ide.eclipse.as.management.core.JBoss7ManagerUtil;
 
+/**
+ * This class is an internal implementation and is not
+ * intended to be subclassed or instantiated by external clients.  
+ */
 public class RSEJBoss7BehaviourDelegate extends RSEBehaviourDelegate {
 
 	private IJBoss7ManagerService service;
