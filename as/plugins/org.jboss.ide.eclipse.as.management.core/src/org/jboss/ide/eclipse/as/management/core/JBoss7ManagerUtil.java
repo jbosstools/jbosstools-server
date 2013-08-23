61a1e6971d8ab202e488de6474fe57242de1a710
Rob Stryker <rob@oxbeef.net>
2013-08-23 19:08:33 +0800

    JBIDE-15272 javadoc updates as per fred's request

--- as/plugins/org.jboss.ide.eclipse.as.management.core/src/org/jboss/ide/eclipse/as/management/core/JBoss7ManagerUtil.java
+++ as/plugins/org.jboss.ide.eclipse.as.management.core/src/org/jboss/ide/eclipse/as/management/core/JBoss7ManagerUtil.java
@@ -17,12 +17,30 @@ import org.eclipse.wst.server.core.ServerCore;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
 
+/**
+ * A utility class for retrieving a IJBoss7ManagerService or 
+ * the id of a IJBoss7ManagerService for a given IServer or IRuntimeType id 
+ */
 public class JBoss7ManagerUtil {
 
+	/**
+	 * Retrieve the appropriate IJBoss7ManagerService for a given IServer
+	 * 
+	 * @param server
+	 * @return
+	 * @throws JBoss7ManangerException
+	 */
 	public static IJBoss7ManagerService getService(IServer server) throws JBoss7ManangerException  {
 		return getService(server.getRuntime().getRuntimeType().getId());
 	}
 	
+	/**
+	 * Retrieve the IJBoss7ManagerService with the given service id. 
+	 * 
+	 * @param serviceId
+	 * @return
+	 * @throws JBoss7ManangerException
+	 */
 	public static IJBoss7ManagerService getManagerService(String serviceId) throws JBoss7ManangerException {
 		try {
 			BundleContext context = AS7ManagementActivator.getContext();
@@ -35,21 +53,46 @@ public class JBoss7ManagerUtil {
 		}
 	}
 
+	/**
+	 * Retrieve the IJBoss7ManagerService for the given runtime type id
+	 * 
+	 * @param runtimeType   the runtimeType id of a org.eclipse.wst.server.core.IRuntimeType
+	 * @return
+	 * @throws JBoss7ManangerException
+	 */
 	public static IJBoss7ManagerService getService(String runtimeType) throws JBoss7ManangerException  {
 		IJBossManagerServiceProvider serviceProvider = getServiceProvider(runtimeType);
 		return serviceProvider == null ? null : serviceProvider.getManagerService();
 	}
 
+	/**
+	 * Get the required management service id for a given IServer
+	 * 
+	 * @param server
+	 * @return
+	 */
 	public static String getRequiredVersion(IServer server) {
 		String id = server.getRuntime().getRuntimeType().getId();
 		return getRequiredServiceVersion(id);
 	}
 	
+	/**
+	 * Get the required management service id for a given IServer
+	 * 
+	 * @param server
+	 * @return
+	 */
 	public static String getRequiredServiceVersion(String runtimeType) {
 		IJBossManagerServiceProvider serviceProvider = getServiceProvider(runtimeType);
 		return serviceProvider == null ? null : serviceProvider.getManagerServiceId();
 	}
 
+	/**
+	 * Get an IJBossManagerServiceProvider for a given runtime type id
+	 * 
+	 * @param runtimeType the runtimeType id of a org.eclipse.wst.server.core.IRuntimeType
+	 * @return
+	 */
 	protected static IJBossManagerServiceProvider getServiceProvider(String runtimeType) {
 		if( runtimeType != null ) {
 			IRuntimeType rtType = ServerCore.findRuntimeType(runtimeType);
@@ -62,12 +105,25 @@ public class JBoss7ManagerUtil {
 		return null;
 	}
 	
+	/**
+	 * Dispose of a management service
+	 * @param service
+	 */
 	public static void dispose(IJBoss7ManagerService service) {
 		if (service != null) {
 			service.dispose();
 		}
 	}
 	
+	/**
+	 * Execute some command, request, or action on the appropriate 
+	 * IJBoss7ManagerService for the given IServer. 
+	 * 
+	 * @param serviceAware	The action to be executed
+	 * @param server   
+	 * @return				
+	 * @throws Exception
+	 */
 	public static <RESULT> RESULT executeWithService(IServiceAware<RESULT> serviceAware, IServer server) throws Exception {
 		IJBoss7ManagerService service = null;
 		try {
@@ -80,6 +136,12 @@ public class JBoss7ManagerUtil {
 		}
 	}
 	
+	/**
+	 * An interface for an object capable of executing a command,
+	 * request, or action on a IJBoss7ManagerService. 
+	 * 
+	 * @param <RESULT> The type of the result you are expecting from the service
+	 */
 	public static interface IServiceAware<RESULT> {
 		public RESULT execute(IJBoss7ManagerService service) throws Exception;
 	}
