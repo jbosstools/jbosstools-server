9d68cbef5851c60d3de449136262fc861e477d5c
Rob Stryker <rob@oxbeef.net>
2013-10-30 20:50:06 +0800

    JBIDE-15357 part 2 with test

---
+++ as/tests/org.jboss.tools.as.test.core/src/org/jboss/tools/as/test/core/parametized/server/publishing/defect/ClosedProjectPublishTest.java
@@ -0,0 +1,104 @@
+/******************************************************************************* 
+ * Copyright (c) 2010 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.jboss.tools.as.test.core.parametized.server.publishing.defect;
+
+import junit.framework.TestCase;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
+import org.eclipse.wst.server.core.IModule;
+import org.eclipse.wst.server.core.IServer;
+import org.eclipse.wst.server.core.IServerWorkingCopy;
+import org.eclipse.wst.server.core.ServerUtil;
+import org.jboss.ide.eclipse.as.core.server.IDeployableServer;
+import org.jboss.ide.eclipse.as.core.util.IJBossToolingConstants;
+import org.jboss.tools.as.test.core.ASMatrixTests;
+import org.jboss.tools.as.test.core.internal.utils.ServerCreationTestUtils;
+import org.jboss.tools.as.test.core.internal.utils.wtp.CreateProjectOperationsUtility;
+import org.jboss.tools.as.test.core.internal.utils.wtp.JavaEEFacetConstants;
+import org.jboss.tools.as.test.core.internal.utils.wtp.OperationTestCase;
+import org.jboss.tools.as.test.core.internal.utils.wtp.ProjectUtility;
+import org.jboss.tools.test.util.JobUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClosedProjectPublishTest extends TestCase {
+	
+	private IServer server;
+	private IModule module;
+	public ClosedProjectPublishTest() {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		String param_serverType = IJBossToolingConstants.SERVER_AS_71;
+		IServer s = ServerCreationTestUtils.createMockServerWithRuntime(param_serverType, getClass().getName() + param_serverType);
+    	
+    	IDataModel dyn1Model = CreateProjectOperationsUtility.getWebDataModel("cpptd1v", null, null, null, null, JavaEEFacetConstants.WEB_23, true);
+    	OperationTestCase.runAndVerify(dyn1Model);
+    	IModule m = ServerUtil.getModule(ResourcesPlugin.getWorkspace().getRoot().getProject("cpptd1v"));
+    	server = s;
+    	module = m;
+    	addOrRemoveModuleWithPublish(new IModule[]{module}, true);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		JobUtils.waitForIdle(100);
+		ServerCreationTestUtils.deleteAllServersAndRuntimes();
+		ProjectUtility.deleteAllProjects();
+		ASMatrixTests.clearStateLocation();
+		JobUtils.waitForIdle();
+	}
+	
+	private void addOrRemoveModuleWithPublish(IModule[] module, boolean add) throws CoreException {
+		IServerWorkingCopy wc = server.createWorkingCopy();
+		if( add )
+			wc.modifyModules(module, new IModule[]{}, new NullProgressMonitor());
+		else
+			wc.modifyModules(new IModule[]{}, module, new NullProgressMonitor());
+		server = wc.save(true, new NullProgressMonitor());
+		server.publish(IServer.PUBLISH_INCREMENTAL, new NullProgressMonitor());
+	}
+	
+	
+	@Test
+	public void testOpenClosedProject() throws Exception {
+		IDeployableServer ds = (IDeployableServer)server.loadAdapter(IDeployableServer.class, null);
+		IPath p = ds.getDeploymentLocation(new IModule[]{module}, true);
+		assertTrue(p.append("WEB-INF").append("web.xml").toFile().exists());
+		
+		module.getProject().close(new NullProgressMonitor() );
+		
+		// publish, verify the contents are still there
+		server.publish(IServer.PUBLISH_INCREMENTAL, new NullProgressMonitor());
+		assertTrue(p.append("WEB-INF").append("web.xml").toFile().exists());
+		
+		server.publish(IServer.PUBLISH_FULL, new NullProgressMonitor());
+		assertTrue(p.append("WEB-INF").append("web.xml").toFile().exists());
+		
+		addOrRemoveModuleWithPublish(new IModule[]{module}, false);
+		
+		assertFalse(p.append("WEB-INF").append("web.xml").toFile().exists());
+		
+		addOrRemoveModuleWithPublish(new IModule[]{module}, true);
+		assertFalse(p.append("WEB-INF").append("web.xml").toFile().exists());
+		module.getProject().open(new NullProgressMonitor() );
+		assertFalse(p.append("WEB-INF").append("web.xml").toFile().exists());
+		server.publish(IServer.PUBLISH_INCREMENTAL, new NullProgressMonitor());
+		assertTrue(p.append("WEB-INF").append("web.xml").toFile().exists());
+		
+	}
+}
