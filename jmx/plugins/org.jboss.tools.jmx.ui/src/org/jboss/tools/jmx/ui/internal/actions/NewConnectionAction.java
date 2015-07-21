30a9d272ac479e8597237251d985f95d595398b0
Rob Stryker <rob@oxbeef.net>
2015-07-20 23:15:19 -0400

    JBIDE-20317 - weird random shell opening during new jmx connection action

--- jmx/plugins/org.jboss.tools.jmx.ui/src/org/jboss/tools/jmx/ui/internal/actions/NewConnectionAction.java
+++ jmx/plugins/org.jboss.tools.jmx.ui/src/org/jboss/tools/jmx/ui/internal/actions/NewConnectionAction.java
@@ -16,6 +16,9 @@ import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.jboss.tools.jmx.ui.Messages;
 import org.jboss.tools.jmx.ui.internal.JMXImages;
 import org.jboss.tools.jmx.ui.internal.wizards.NewConnectionWizard;
@@ -33,7 +36,9 @@ public class NewConnectionAction extends Action {
 		Display.getDefault().asyncExec(new Runnable() {
 			public void run() {
 				IWizard wizard = new NewConnectionWizard();
-				WizardDialog d = new WizardDialog(new Shell(), wizard);
+				IWorkbench wb = PlatformUI.getWorkbench();
+				IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+				WizardDialog d = new WizardDialog(win.getShell(), wizard);
 				d.open();
 			}
 		} );
