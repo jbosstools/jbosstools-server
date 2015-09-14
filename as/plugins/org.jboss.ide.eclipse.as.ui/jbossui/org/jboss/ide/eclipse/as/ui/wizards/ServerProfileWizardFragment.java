8a01f0cae37b0982201f7cc43beee098812f46f5
Rob Stryker <rob@oxbeef.net>
2015-09-14 14:26:23 -0400

    JBIDE-20667 - new server wizard has 'finish' enabled on first page

--- as/plugins/org.jboss.ide.eclipse.as.ui/jbossui/org/jboss/ide/eclipse/as/ui/wizards/ServerProfileWizardFragment.java
+++ as/plugins/org.jboss.ide.eclipse.as.ui/jbossui/org/jboss/ide/eclipse/as/ui/wizards/ServerProfileWizardFragment.java
@@ -139,7 +139,7 @@ public class ServerProfileWizardFragment extends WizardFragment implements IComp
 	 * the description label. 
 	 */
 	protected boolean hasDisposedWidgets() {
-		return serverExplanationLabel != null && serverExplanationLabel.isDisposed();
+		return serverExplanationLabel == null || serverExplanationLabel.isDisposed();
 	}
 	
 	
