4ada52460f38ff953f7c2417cc7e0756dceb145a
Rob Stryker <rob@oxbeef.net>
2015-11-04 01:22:53 -0500

    Adding a new constructor to server editor widget used to make combo widgets respond to undo command. This new constructor is used for read-only combo boxes with selection listeners instead of modify listeners

--- as/plugins/org.jboss.ide.eclipse.as.wtp.ui/src/org/jboss/ide/eclipse/as/wtp/ui/editor/ServerWorkingCopyPropertyComboCommand.java
+++ as/plugins/org.jboss.ide.eclipse.as.wtp.ui/src/org/jboss/ide/eclipse/as/wtp/ui/editor/ServerWorkingCopyPropertyComboCommand.java
@@ -13,6 +13,7 @@ package org.jboss.ide.eclipse.as.wtp.ui.editor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.wst.server.core.IServerWorkingCopy;
 import org.eclipse.wst.server.ui.internal.command.ServerCommand;
@@ -27,7 +28,8 @@ public class ServerWorkingCopyPropertyComboCommand extends ServerCommand {
 	protected String newVal;
 	protected String key;
 	protected Combo combo;
-	protected ModifyListener listener;
+	protected ModifyListener modlistener;
+	protected SelectionListener selListener;
 	protected IServerWorkingCopy wc;
 	
 	public ServerWorkingCopyPropertyComboCommand(IServerWorkingCopy wc, String commandName, 
@@ -37,7 +39,19 @@ public class ServerWorkingCopyPropertyComboCommand extends ServerCommand {
 		this.combo = combo;
 		this.key = attributeKey;
 		this.newVal = newVal;
-		this.listener = listener;
+		this.modlistener = listener;
+		if( key != null )
+			this.oldVal = wc.getAttribute(attributeKey, ""); //$NON-NLS-1$
+	}
+	
+	public ServerWorkingCopyPropertyComboCommand(IServerWorkingCopy wc, String commandName, 
+			Combo combo, String newVal, String attributeKey, SelectionListener listener) {
+		super(wc, commandName);
+		this.wc = wc;
+		this.combo = combo;
+		this.key = attributeKey;
+		this.newVal = newVal;
+		this.selListener = listener;
 		if( key != null )
 			this.oldVal = wc.getAttribute(attributeKey, ""); //$NON-NLS-1$
 	}
@@ -48,8 +62,11 @@ public class ServerWorkingCopyPropertyComboCommand extends ServerCommand {
 	}
 	
 	public void undo() {
-		if( listener != null )
-			combo.removeModifyListener(listener);
+		if( modlistener != null )
+			combo.removeModifyListener(modlistener);
+		if( selListener != null )
+			combo.removeSelectionListener(selListener);
+
 		wc.setAttribute(key, oldVal);
 		if( combo != null && !combo.isDisposed()) {
 			String oldValAsText = getStringForValue(oldVal);
@@ -60,8 +77,10 @@ public class ServerWorkingCopyPropertyComboCommand extends ServerCommand {
 				combo.setText(oldValAsText);
 			}
 		}
-		if( listener != null )
-			combo.addModifyListener(listener);
+		if( modlistener != null )
+			combo.addModifyListener(modlistener);
+		if( selListener != null )
+			combo.addSelectionListener(selListener);
 		postOp(POST_UNDO);
 	}
 
@@ -75,8 +94,10 @@ public class ServerWorkingCopyPropertyComboCommand extends ServerCommand {
 	}
 	
 	public IStatus redo() {
-		if( listener != null )
-			combo.removeModifyListener(listener);
+		if( modlistener != null )
+			combo.removeModifyListener(modlistener);
+		if( selListener != null )
+			combo.removeSelectionListener(selListener);
 		wc.setAttribute(key, newVal);
 		if( combo != null && !combo.isDisposed()) {
 			String newValAsText = getStringForValue(newVal);
@@ -87,8 +108,10 @@ public class ServerWorkingCopyPropertyComboCommand extends ServerCommand {
 				combo.setText(newValAsText);
 			}
 		}
-		if( listener != null )
-			combo.addModifyListener(listener);
+		if( modlistener != null )
+			combo.addModifyListener(modlistener);
+		if( selListener != null )
+			combo.addSelectionListener(selListener);
 		postOp(POST_REDO);
 		return Status.OK_STATUS;
 	}
