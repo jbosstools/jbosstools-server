0ce74780a61f8a74fb92e7c8dc9a94720b39a245
rhopp <rhopp@redhat.com>
2016-01-18 14:20:43 +0100

    JBIDE-21470 - MessageBox changed to MessageDialog in ZombieProcessPrompter

--- as/plugins/org.jboss.ide.eclipse.as.wtp.ui/src/org/jboss/ide/eclipse/as/wtp/ui/prompt/ZombieProcessPrompter.java
+++ as/plugins/org.jboss.ide.eclipse.as.wtp.ui/src/org/jboss/ide/eclipse/as/wtp/ui/prompt/ZombieProcessPrompter.java
@@ -10,10 +10,9 @@
  ******************************************************************************/ 
 package org.jboss.ide.eclipse.as.wtp.ui.prompt;
 
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.osgi.util.NLS;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
@@ -41,21 +40,9 @@ public class ZombieProcessPrompter implements IPromptHandler {
 	private static Object promptUserZombieProcess(Object... data) {
 		IWorkbenchWindow active = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 		Shell s = active.getShell();
-        MessageBox messageBox = new MessageBox(s,
-        		SWT.ICON_WARNING | SWT.YES | SWT.NO);
-        
-        messageBox.setText("Warning: server process not terminated");
-        messageBox.setMessage(NLS.bind(
+		return MessageDialog.openQuestion(s, "Warning: server process not terminated", NLS.bind(
         		"The process for server {0} has not been terminated. Would you like us to terminate it for you?", 
         		((IServer)data[0]).getName()));
-        int buttonID = messageBox.open();
-        switch(buttonID) {
-          case SWT.YES:
-        	  return true;
-          case SWT.NO:
-        	  return false;
-        }
-        return null;
 	}
 
 }
