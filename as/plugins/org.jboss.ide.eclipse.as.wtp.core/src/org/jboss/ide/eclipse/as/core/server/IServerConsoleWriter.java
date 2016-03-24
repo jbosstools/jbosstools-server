285e64afc1b765fdb74c4917d71693c40d9c21ca
Xavier Coulon <xcoulon@redhat.com>
2016-03-24 17:28:03 +0100

    JBIDE-21949 - Allow user to turn on/off showing console when output changes
    Changing the code to show the message console when it is created only,
    then offering two methods to write lines in the shell and activate the
    view or not (default is 'true').
    
    Also, upgrading the project's execution environment to Java 1.7 to take
    advantage of the 'try-with-resources' statement to make sure that the
    outputstream is closed upon writting.
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- as/plugins/org.jboss.ide.eclipse.as.wtp.core/src/org/jboss/ide/eclipse/as/core/server/IServerConsoleWriter.java
+++ as/plugins/org.jboss.ide.eclipse.as.wtp.core/src/org/jboss/ide/eclipse/as/core/server/IServerConsoleWriter.java
@@ -10,6 +10,28 @@
  ******************************************************************************/ 
 package org.jboss.ide.eclipse.as.core.server;
 
+/**
+ * 
+ */
 public interface IServerConsoleWriter extends IJBASHostShellListener {
+	
+	/**
+	 * Writes the given {@code lines} into the {@link MessageConsole} associated with the given {@code serverId}.
+	 * If the {@link MessageConsole} did not exist yet, it is created and shown. Calling this method will activate
+	 * the console while the lines are written.
+	 * 
+	 * @param serverId the id of the server associated with this console.
+	 * @param lines the lines to write in the console.
+	 */
 	public void writeToShell(String serverId, String[] lines);
+	
+	/**
+	 * Writes the given {@code lines} into the {@link MessageConsole} associated with the given {@code serverId}.
+	 * If the {@link MessageConsole} did not exist yet, it is created and shown.
+	 * @param serverId the id of the server associated with this console.
+	 * @param lines the lines to write in the console.
+	 * @param activateOnWrite if the view should be activated when the line are written in the console.
+	 */
+	public void writeToShell(final String serverId, final String[] lines, final boolean activateOnWrite);
+	
 }
