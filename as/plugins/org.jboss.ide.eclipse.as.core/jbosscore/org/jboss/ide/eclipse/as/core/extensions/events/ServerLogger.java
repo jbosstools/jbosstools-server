d82e9f9b649b4cca276d367fde162eedad73e5f6
Rob Stryker <rob@oxbeef.net>
2017-03-10 15:44:04 -0500

    JBIDE-22717 - Remvoe Event Log

--- as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/extensions/events/ServerLogger.java
+++ as/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/extensions/events/ServerLogger.java
@@ -10,20 +10,9 @@
  ******************************************************************************/ 
 package org.jboss.ide.eclipse.as.core.extensions.events;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.preferences.IEclipsePreferences;
-import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.wst.server.core.IServer;
-import org.eclipse.wst.server.core.ServerCore;
 import org.jboss.ide.eclipse.as.core.JBossServerCorePlugin;
-import org.jboss.ide.eclipse.as.core.server.IServerLogListener;
-import org.jboss.ide.eclipse.as.core.util.IJBossToolingConstants;
-import org.osgi.service.prefs.BackingStoreException;
 
 /**
  * This class is marked for deletion. There is no alternative. 
@@ -31,7 +20,6 @@ import org.osgi.service.prefs.BackingStoreException;
  */
 @Deprecated
 public class ServerLogger {
-	private static final String RELOG_ERROR_PREF_KEY = "package org.jboss.ide.eclipse.as.core.extensions.events.ServerLogger.RELOG_ERROR_PREF_KEY"; //$NON-NLS-1$
 	private static ServerLogger instance;
 	public static ServerLogger getDefault() {
 		if( instance == null ) {
@@ -39,84 +27,8 @@ public class ServerLogger {
 		}
 		return instance;
 	}
-	
-	private HashMap<String, ServerLog> map = new HashMap<String, ServerLog>();
-	private HashMap<String, ArrayList<IServerLogListener>> listeners = 
-		new HashMap<String, ArrayList<IServerLogListener>>();
-	
-	public void addListener(IServer server, IServerLogListener listener) {
-		ArrayList<IServerLogListener> list = listeners.get(server.getId());
-		if( list == null ) {
-			list = new ArrayList<IServerLogListener>();
-			listeners.put(server.getId(), list);
-		}
-		list.add(listener);
-	}
-	
-	public void removeListener(IServer server, IServerLogListener listener) {
-		ArrayList<IServerLogListener> list = listeners.get(server.getId());
-		if( list != null ) {
-			list.remove(listener);
-		}
-	}
-	
-	public void log(IServer server, IStatus status) {
-		ServerLog log = map.get(server.getId());
-		if( log == null ) {
-			log = new ServerLog(getServerLogFile(server));
-			map.put(server.getId(), log);
-		}
-		log.log(status);
-		if( shouldDoubleLogErrors() && status.getSeverity() == IStatus.ERROR) {
-			JBossServerCorePlugin.getDefault().getLog().log(status);
-		}
 		
-		ArrayList<IServerLogListener> list = listeners.get(server.getId());
-		if( list != null ) {
-			IServerLogListener[] listeners = 
-				list.toArray(new IServerLogListener[list.size()]);
-			for( int i = 0; i < listeners.length;i++) 
-				listeners[i].logging(status, server);
-		}
-	}
-	
-	public IStatus[] getLog(IServer server) {
-		ServerLog log = map.get(server.getId());
-		if( log == null )
-			return new IStatus[]{};
-		return log.getLogStatusObjects();
-	}
-	
-	public static boolean shouldDoubleLogErrors() {
-		IEclipsePreferences prefs = new InstanceScope().getNode(JBossServerCorePlugin.PLUGIN_ID);
-		return prefs.getBoolean(RELOG_ERROR_PREF_KEY, true);
-	}
-	
-	public static void setDoubleLogErrors(boolean val) {
-		IEclipsePreferences prefs = new InstanceScope().getNode(JBossServerCorePlugin.PLUGIN_ID);
-		prefs.putBoolean(RELOG_ERROR_PREF_KEY, val);
-		try {
-			prefs.flush();
-		} catch( BackingStoreException bse) {
-			JBossServerCorePlugin.log(new Status(IStatus.WARNING, JBossServerCorePlugin.PLUGIN_ID, bse.getMessage(), bse));
-		}
-	}
-	
-	public static File getServerLogFile(IServer server) {
-		File f = JBossServerCorePlugin.getServerStateLocation(
-				server).append(IJBossToolingConstants.LOG).toFile();
-		if( !f.getParentFile().exists() ) 
-			f.getParentFile().mkdirs();
-		return f;
-	}
-	
-	public static IServer findServerForFile(File file) {
-		if( file == null )
-			return null;
-		IServer[] servers = ServerCore.getServers();
-		for( int i = 0; i < servers.length; i++ )
-			if(getServerLogFile(servers[i]).equals(file))
-				return servers[i];
-		return null;
+	public void log(IServer server, IStatus status) {
+		JBossServerCorePlugin.getDefault().getLog().log(status);
 	}
 }
