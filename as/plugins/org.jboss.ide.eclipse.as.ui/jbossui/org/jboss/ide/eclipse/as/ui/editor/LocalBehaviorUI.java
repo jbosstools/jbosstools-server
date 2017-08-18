61c28dc3c76215f461c46a3e007e22ad83d7dbe8
Rob Stryker <rob@oxbeef.net>
2017-08-18 15:50:00 -0400

    JBIDE-24758 - update server editor status after changes to runtime

--- as/plugins/org.jboss.ide.eclipse.as.ui/jbossui/org/jboss/ide/eclipse/as/ui/editor/LocalBehaviorUI.java
+++ as/plugins/org.jboss.ide.eclipse.as.ui/jbossui/org/jboss/ide/eclipse/as/ui/editor/LocalBehaviorUI.java
@@ -15,8 +15,14 @@ import java.beans.PropertyChangeListener;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.wst.server.core.IRuntime;
+import org.jboss.ide.eclipse.as.core.server.UnitedServerListener;
+import org.jboss.ide.eclipse.as.core.server.UnitedServerListenerManager;
 import org.jboss.ide.eclipse.as.core.server.internal.ExtendedServerPropertiesAdapterFactory;
 import org.jboss.ide.eclipse.as.core.server.internal.extendedproperties.ServerExtendedProperties;
 import org.jboss.ide.eclipse.as.wtp.core.server.behavior.ServerProfileModel;
@@ -29,13 +35,42 @@ public class LocalBehaviorUI implements IDeploymentTypeUI {
 		callback.setErrorMessage(null);
 		if( callback.getCallbackType() == IServerModeUICallback.EDITOR)
 			verify();
-		callback.getServer().addPropertyChangeListener(new PropertyChangeListener(){
+		parent.setLayout(new FillLayout());
+		Composite child = new Composite(parent, SWT.None);
+		
+		addListeners(parent);
+	}
+	
+	private void addListeners(Composite parent) {
+		// Add listeners for the server
+		final PropertyChangeListener pcl = new PropertyChangeListener(){
 			public void propertyChange(PropertyChangeEvent evt) {
 				if( callback.getCallbackType() == IServerModeUICallback.EDITOR)
 					verify();
-			}});
-		parent.setLayout(new FillLayout());
-		Composite child = new Composite(parent, SWT.None);
+			}
+		};
+		callback.getServer().addPropertyChangeListener(pcl);
+		
+		
+		// Add listeners for the runtime
+		final UnitedServerListener changeToRuntimeDetailsListener = new UnitedServerListener() {
+			public void runtimeChanged(IRuntime runtime) {
+				if( callback.getCallbackType() == IServerModeUICallback.EDITOR) {
+					Display.getDefault().asyncExec(() -> verify());
+				}
+			}
+			public boolean canHandleRuntime(IRuntime runtime) {
+				return runtime != null && runtime.equals(callback.getServer().getRuntime());
+			}
+		};
+		UnitedServerListenerManager.getDefault().addListener(changeToRuntimeDetailsListener);
+		parent.addDisposeListener(new DisposeListener() {
+			@Override
+			public void widgetDisposed(DisposeEvent e) {
+				UnitedServerListenerManager.getDefault().removeListener(changeToRuntimeDetailsListener);
+				callback.getServer().removePropertyChangeListener(pcl);
+			}
+		});
 	}
 	
 	private void verify() {
