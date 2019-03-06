1eedbab881fc61d14c1facb7f1ee4c858bda41a4
Josef Kopriva <jkopriva@redhat.com>
2019-03-06 08:10:19 +0100

    JBIDE-26568 Add WF16 and EAP72 in itests suites
    Signed-off-by: Josef Kopriva <jkopriva@redhat.com>

--- as/itests/org.jboss.tools.as.ui.bot.itests/src/org/jboss/tools/as/ui/bot/itests/parametized/server/ServerRuntimeUIConstants.java
+++ as/itests/org.jboss.tools.as.ui.bot.itests/src/org/jboss/tools/as/ui/bot/itests/parametized/server/ServerRuntimeUIConstants.java
@@ -45,6 +45,7 @@ public class ServerRuntimeUIConstants {
 	public static final String WF_13_0_0 = "WildFly 13.0.0 Final";
 	public static final String WF_14_0_0 = "WildFly 14.0.0 Final";
 	public static final String WF_15_0_0 = "WildFly 15.0.0 Final";
+	public static final String WF_16_0_0 = "WildFly 16.0.0 Final";
 
 	// Requires credentials
 	// public static final String JBEAP_610 = "JBoss EAP 6.1.0";
@@ -53,6 +54,7 @@ public class ServerRuntimeUIConstants {
 	public static final String JBEAP_640 = "JBoss EAP 6.4.0";
 	public static final String JBEAP_700 = "JBoss EAP 7.0.0";
 	public static final String JBEAP_710 = "JBoss EAP 7.1.0";
+	public static final String JBEAP_720 = "JBoss EAP 7.2.0";
 	public static final String JPP_610 = "JBoss Portal Platform 6.1.0";
 
 	
@@ -64,21 +66,21 @@ public class ServerRuntimeUIConstants {
 	
 	public static final String[] LATEST_MAJORS_FREE_DOWNLOADS = new String[] {
 			GATE_IN_3_6, JBAS_423, JBAS_510, JBAS_600, JBAS_711, 
-			WF_821, WF_902, WF_10_1_0, WF_11_0_0, WF_12_0_0, WF_13_0_0, WF_14_0_0, WF_15_0_0};
+			WF_821, WF_902, WF_10_1_0, WF_11_0_0, WF_12_0_0, WF_13_0_0, WF_14_0_0, WF_15_0_0, WF_16_0_0};
 
 	
 	public static final String[] FREE_DOWNLOADS = new String[] {
-			WF_821, WF_902, WF_10_1_0, WF_11_0_0, WF_12_0_0, WF_13_0_0, WF_14_0_0, WF_15_0_0}; // 
+			WF_821, WF_902, WF_10_1_0, WF_11_0_0, WF_12_0_0, WF_13_0_0, WF_14_0_0, WF_15_0_0, WF_16_0_0}; // 
 
 //	public static final String[] MANUAL_DOWNLOAD = new String[]{JPP_600,JBEAP_600, JBEAP_601};
 
 	public static final String[] ZERO_DOLLAR = new String[] { 
-			JBEAP_640, JBEAP_700, JBEAP_710};
+			JBEAP_640, JBEAP_700, JBEAP_710, JBEAP_720};
 
 	public static final String[] ALL_DOWNLOADS = Stream
 			.concat(Arrays.stream(FREE_DOWNLOADS), Arrays.stream(ZERO_DOLLAR)).toArray(String[]::new);
 
-	public static final String[] SMOKETEST_DOWNLOADS = new String[] { JBEAP_710, WF_15_0_0};
+	public static final String[] SMOKETEST_DOWNLOADS = new String[] { JBEAP_720, WF_16_0_0};
 
 	private static HashMap<String, ModelWrapper> expectations = null;
 
@@ -116,12 +118,14 @@ public class ServerRuntimeUIConstants {
 		addEntry(WF_13_0_0, 	"WildFly 13.0",			"13.0", "WildFly", 	"wildfly-13.0.0.Final",			STANDARD_DEPLOY, STANDARD_UNDEPLOY, wfEditorPorts());
 		addEntry(WF_14_0_0, 	"WildFly 14.0",			"14.0", "WildFly", 	"wildfly-14.0.0.Final",			STANDARD_DEPLOY, STANDARD_UNDEPLOY, wfEditorPorts());
 		addEntry(WF_15_0_0, 	"WildFly 15.0",			"15.0", "WildFly", 	"wildfly-15.0.0.Final",			STANDARD_DEPLOY, STANDARD_UNDEPLOY, wfEditorPorts());
+		addEntry(WF_16_0_0, 	"WildFly 16.0",			"16.0", "WildFly", 	"wildfly-16.0.0.Final",			STANDARD_DEPLOY, STANDARD_UNDEPLOY, wfEditorPorts());
 		addEntry(JBEAP_610, 	"Red Hat JBoss EAP 6.1", "6.1",	"EAP", 		"jboss-eap-6.1", 				STANDARD_DEPLOY, STANDARD_UNDEPLOY, as7EditorPorts());
 		addEntry(JBEAP_620, 	"Red Hat JBoss EAP 6.2", "6.2",	"EAP", 		"jboss-eap-6.2", 				STANDARD_DEPLOY, STANDARD_UNDEPLOY, as7EditorPorts());
 		addEntry(JBEAP_630, 	"Red Hat JBoss EAP 6.3", "6.3",	"EAP",		"jboss-eap-6.3", 				STANDARD_DEPLOY, STANDARD_UNDEPLOY, as7EditorPorts());
 		addEntry(JBEAP_640, 	"Red Hat JBoss EAP 6.4", "6.4",	"EAP", 		"jboss-eap-6.4", 				STANDARD_DEPLOY, STANDARD_UNDEPLOY, as7EditorPorts());
 		addEntry(JBEAP_700, 	"Red Hat JBoss EAP 7.0", "7.0",	"EAP", 		"jboss-eap-7.0", 				STANDARD_DEPLOY, STANDARD_UNDEPLOY, wfEditorPorts());
 		addEntry(JBEAP_710, 	"Red Hat JBoss EAP 7.1", "7.1",	"EAP", 		"jboss-eap-7.1", 				STANDARD_DEPLOY, STANDARD_UNDEPLOY, wfEditorPorts());
+		addEntry(JBEAP_720, 	"Red Hat JBoss EAP 7.2", "7.2",	"EAP", 		"jboss-eap-7.2", 				STANDARD_DEPLOY, STANDARD_UNDEPLOY, wfEditorPorts());
 		addEntry(JPP_610, 		"JBoss Portal 6.1",		"6.1", "JPP", 		"jboss-jpp-6.1", 				STANDARD_DEPLOY, STANDARD_UNDEPLOY, as7EditorPorts());
 		
 		
