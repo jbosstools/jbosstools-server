dd14f1ab328c2676206dcf056f136a97c6c6aa09
Josef Kopriva <jkopriva@redhat.com>
2019-11-14 13:55:43 +0100

    New ServerAdaptersTest (JBIDE-26853) (#647)
    (Added copyright in some test classes)
    
    Signed-off-by: Josef Kopriva <jkopriva@redhat.com>

--- as/itests/org.jboss.tools.as.ui.bot.itests/src/org/jboss/tools/as/ui/bot/itests/AllTestsSuite.java
+++ as/itests/org.jboss.tools.as.ui.bot.itests/src/org/jboss/tools/as/ui/bot/itests/AllTestsSuite.java
@@ -1,3 +1,13 @@
+  /*******************************************************************************
+ * Copyright (c) 2007-2019 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v 1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.as.ui.bot.itests;
 
 import org.eclipse.reddeer.junit.runner.RedDeerSuite;
@@ -5,6 +15,7 @@ import org.jboss.tools.as.ui.bot.itests.archives.DeployingArchiveTest;
 import org.jboss.tools.as.ui.bot.itests.archives.VariousProjectsArchiving;
 import org.jboss.tools.as.ui.bot.itests.download.InvalidCredentialProductDownloadTest;
 import org.jboss.tools.as.ui.bot.itests.parametized.server.RuntimeDetectionDuplicatesTest;
+import org.jboss.tools.as.ui.bot.itests.parametized.server.ServerAdaptersTest;
 import org.jboss.tools.as.ui.bot.itests.parametized.server.ServerRuntimesTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -16,6 +27,7 @@ import org.junit.runners.Suite;
 	VariousProjectsArchiving.class,
 	DeployingArchiveTest.class,
 	RuntimeDetectionDuplicatesTest.class,
+	ServerAdaptersTest.class
 })
 public class AllTestsSuite {
 
