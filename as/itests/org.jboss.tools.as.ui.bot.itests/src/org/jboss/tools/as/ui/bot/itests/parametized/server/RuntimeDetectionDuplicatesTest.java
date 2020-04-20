e0e44451e10df764b2d53e7ddf7b1195f117327d
Rob Stryker <rob@oxbeef.net>
2020-04-20 11:12:13 -0400

    Add missing headers
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- as/itests/org.jboss.tools.as.ui.bot.itests/src/org/jboss/tools/as/ui/bot/itests/parametized/server/RuntimeDetectionDuplicatesTest.java
+++ as/itests/org.jboss.tools.as.ui.bot.itests/src/org/jboss/tools/as/ui/bot/itests/parametized/server/RuntimeDetectionDuplicatesTest.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ * 
+ * Contributors: Red Hat, Inc.
+ ******************************************************************************/
 package org.jboss.tools.as.ui.bot.itests.parametized.server;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
