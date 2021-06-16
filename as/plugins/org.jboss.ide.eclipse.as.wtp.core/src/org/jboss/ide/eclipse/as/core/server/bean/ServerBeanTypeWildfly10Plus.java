4399e7d336fe4b5c962e4c4864a95f42d8781fda
Rob Stryker <rob@oxbeef.net>
2021-06-16 14:50:55 -0400

    Allow wfly 24 adapter to be used for future versions
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- as/plugins/org.jboss.ide.eclipse.as.wtp.core/src/org/jboss/ide/eclipse/as/core/server/bean/ServerBeanTypeWildfly10Plus.java
+++ as/plugins/org.jboss.ide.eclipse.as.wtp.core/src/org/jboss/ide/eclipse/as/core/server/bean/ServerBeanTypeWildfly10Plus.java
@@ -11,6 +11,10 @@
 package org.jboss.ide.eclipse.as.core.server.bean;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.jboss.ide.eclipse.as.core.util.IJBossToolingConstants;
 
 public class ServerBeanTypeWildfly10Plus extends JBossServerType {
 	private static final String WF_100_RELEASE_MANIFEST_KEY = "JBoss-Product-Release-Version"; //$NON-NLS-1$
@@ -47,13 +51,59 @@ public class ServerBeanTypeWildfly10Plus extends JBossServerType {
 		public String getFullVersion(File location, File systemFile) {
 			String vers = ServerBeanType.getManifestPropFromJBossModulesFolder(new File[]{new File(location, "modules")}, 
 					"org.jboss.as.product", productFolder + "/dir/META-INF", WF_100_RELEASE_MANIFEST_KEY);
-			if( vers != null && vers.startsWith(version4Char.substring(0,3))) {
-				return vers;
-			}
-			return null;
+			return getFullVersionWildFly10Impl(vers, this.version4Char, this.serverTypeId);
 		}
 
+		public static String getFullVersionWildFly10Impl(String vers, 
+				String version4Char, String serverTypeId) {
+			if( vers == null )
+				return null;
+			
+			String targetMajorMinor = vers.substring(0,2) + vers.substring(3,4);
+			String conditionMajorMinor = version4Char.substring(0,2) + version4Char.substring(3,4);
+			
+			int conditionMajorMinorInt = Integer.parseInt(conditionMajorMinor);
+			int targetMajorMinorInt = Integer.parseInt(targetMajorMinor);
+			if( conditionMajorMinorInt < 240 || targetMajorMinorInt < 240 ) {
+				// Old logic
+				if( vers.startsWith(version4Char.substring(0,3))) {
+					return vers;
+				}
+				return null;
+			}
+			
+			if(isMyResponsibility(conditionMajorMinorInt, targetMajorMinorInt, serverTypeId)) {
+				return vers;
+			}
+			
+			return null;
+		}
 		
+		private static boolean isMyResponsibility(int conditionMajorMinorInt, 
+				int targetMajorMinorInt, String serverTypeId) {
+			// Find all wildfly server types above me
+			ArrayList<Integer> possibleAdapterVersions = new ArrayList<>();
+			String[] serverTypes = IJBossToolingConstants.ALL_JBOSS_SERVERS;
+			String v = null;
+			for( int i = 0; i < serverTypes.length; i++ ) {
+				if( serverTypes[i].startsWith(IJBossToolingConstants.WF_SERVER_PREFIX)) {
+					v = serverTypes[i].substring(IJBossToolingConstants.WF_SERVER_PREFIX.length());
+					int vInt = Integer.parseInt(v);
+					if( vInt >= conditionMajorMinorInt && vInt <= targetMajorMinorInt)
+						possibleAdapterVersions.add(vInt);
+				}
+			}
+			
+			Collections.sort(possibleAdapterVersions);
+			Collections.reverse(possibleAdapterVersions);
+			if( possibleAdapterVersions.size() == 0 ) 
+				return false;
+			int last = possibleAdapterVersions.get(0);
+			if( (IJBossToolingConstants.WF_SERVER_PREFIX + last).equals(serverTypeId))
+				return true;
+			return false;
+		}
+
 		public boolean isServerRoot(File location) {
 			return getFullVersion(location, null) != null;
 		}
