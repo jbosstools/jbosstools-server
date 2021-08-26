beba0ea819f5063c59d8ec403b2faa56ee2508c5
lgrossma <86789336+lgrossma@users.noreply.github.com>
2021-08-26 12:04:49 +0200

    Add show in context menu test
    Signed-off-by: lgrossma <86789336+lgrossma@users.noreply.github.com>

---
+++ as/itests/org.jboss.tools.as.ui.bot.itests/src/org/jboss/tools/as/ui/bot/itests/SmokeTestsSuite.java
@@ -0,0 +1,29 @@
+ /*******************************************************************************
+ * Copyright (c) 2007-2021 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v 1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.as.ui.bot.itests;
+
+import org.eclipse.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.as.ui.bot.itests.parametized.server.ShowInContextMenuTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @auth Lukas Grossmann
+ *
+ */
+@RunWith(RedDeerSuite.class)
+@Suite.SuiteClasses({ 
+	ShowInContextMenuTest.class
+})
+
+public class SmokeTestsSuite {
+
+}
