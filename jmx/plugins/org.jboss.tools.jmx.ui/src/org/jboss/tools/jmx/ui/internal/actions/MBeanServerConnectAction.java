9521d6d6ed86a6d6a12ec10a79d9ffb5098f52f7
Rob Stryker <rob@oxbeef.net>
2023-05-30 16:56:34 -0400

    Fixes #793 - add telemetry for missing feature-parity features
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- jmx/plugins/org.jboss.tools.jmx.ui/src/org/jboss/tools/jmx/ui/internal/actions/MBeanServerConnectAction.java
+++ jmx/plugins/org.jboss.tools.jmx.ui/src/org/jboss/tools/jmx/ui/internal/actions/MBeanServerConnectAction.java
@@ -12,16 +12,25 @@
 package org.jboss.tools.jmx.ui.internal.actions;
 
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.eclipse.jface.action.Action;
 import org.jboss.tools.jmx.core.ConnectJob;
 import org.jboss.tools.jmx.core.IConnectionWrapper;
+import org.jboss.tools.jmx.ui.JMXUIActivator;
 import org.jboss.tools.jmx.ui.Messages;
 import org.jboss.tools.jmx.ui.internal.JMXImages;
+import org.jboss.tools.usage.event.UsageEventType;
+import org.jboss.tools.usage.event.UsageReporter;
 
 /**
  * The connect action
  */
 public class MBeanServerConnectAction extends Action {
+	private static UsageEventType EVENT_TYPE  = null;
+	
 	private IConnectionWrapper[] connection;
     public MBeanServerConnectAction(IConnectionWrapper[] wrapper) {
         super(Messages.MBeanServerConnectAction_text, AS_PUSH_BUTTON);
@@ -31,7 +40,27 @@ public class MBeanServerConnectAction extends Action {
 
 	public void run() {
 		if( connection != null ) {
+			logJmxUsage(connection);
 			new ConnectJob(connection).schedule();
 		}
     }
+	
+	private void logJmxUsage(IConnectionWrapper[] connection) {
+		if( EVENT_TYPE == null ) {
+			EVENT_TYPE = new UsageEventType("jmx", UsageEventType.getVersion(JMXUIActivator.getDefault()), 
+					null, "jmx_connect", "JMX Provider type", 
+					"JMX Provider type");
+			UsageReporter.getInstance().registerEvent(EVENT_TYPE);
+		}
+		
+		try {
+			String providers = Arrays.asList(connection).stream().map(val -> {
+				return val == null || val.getProvider() == null || val.getProvider().getId() == null ? "null" : val.getProvider().getId();
+			}).collect(Collectors.joining(","));
+			UsageReporter.getInstance().trackEvent(EVENT_TYPE.event(providers));
+		} catch( Throwable t ) {
+			// Do nothing
+		}
+	}
+
 }
